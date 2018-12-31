af878c03d6e0b06f22bbb71646fc2da9913b5c62
davidbestue <davidsanchezbestue@hotmail.com>
2018-12-31 20:44:54 +0100

    Update loop_se_sess_analysis_2_0.py

--- scripts/loop_se_sess_analysis_2_0.py
+++ scripts/loop_se_sess_analysis_2_0.py
@@ -149,7 +149,7 @@ for SUBJECT_USE_ANALYSIS in ['d001', 'n001', 'r001', 'b001', 'l001', 's001']:
                     
                     
                 WM_delay=[]
-                nscans_wm = 12/2 #9
+                nscans_wm = 12 #9
                 Behaviour=[]
                 
                 headers_col = ['type', 'delay1', 'delay2', 'T', 'NT1', 'NT2', 'Dist', 'Dist_NT1', 'Dist_NT2', 'distance_T_dist', 'cue', 'order',
@@ -282,7 +282,7 @@ for SUBJECT_USE_ANALYSIS in ['d001', 'n001', 'r001', 'b001', 'l001', 's001']:
                     #List to append all the trial channels
                     channels_trial=[]
                     
-                    for time_scan in range(0, shape(Subset)[1], 2 ):
+                    for time_scan in range(0, nscans_wm/2, 2 ):
                         #Get the activity of the TR in the trial        
                         #Signal = Subset[trial,time_scan,:]
                         Signal = array([Subset[trial,time_scan,:], Subset[trial,time_scan + 1,:]]).mean(axis=0)
@@ -324,7 +324,7 @@ for SUBJECT_USE_ANALYSIS in ['d001', 'n001', 'r001', 'b001', 'l001', 's001']:
                 #Heatmap
                 plt.figure()
                 df = pd.DataFrame()
-                for i in range(0,9):
+                for i in range(0,nscans_wm/2):
                     n = list(Channel_all_trials_rolled[:,i,:].mean(axis=0))
                     #n.reverse()
                     df[str( round(2.335*i, 2)  )] = n
