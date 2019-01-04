7415dc32020661acf031fd3b0ff8c28f38a510f5
davidbestue <davidsanchezbestue@hotmail.com>
2019-01-04 13:12:55 +0100

    Update combine_subjects.py

--- scripts/combine_subjects.py
+++ scripts/combine_subjects.py
@@ -6,13 +6,20 @@ Created on Fri Jan  4 11:56:47 2019
 """
 
 
+root = '/home/david/Desktop/KAROLINSKA/bysess_mix_2TR/Conditions/'
+
+
+xls = pd.ExcelFile(Matrix_results_name)
+# Now you can list all sheets in the file
+xls.sheet_names
 
 
 for SUBJECT_USE_ANALYSIS in ['d001', 'n001', 'r001', 'b001', 'l001', 's001']:
     for algorithm in ["visual", "ips"]:  
         Method_analysis = 'bysess'
+        distance='mix'
         CONDITION = '1_0.2' #'1_0.2', '1_7', '2_0.2', '2_7'
         
         ## Load Results
-        Matrix_weights_name = SUBJECT_USE_ANALYSIS + '_' + algorithm + '_' + Method_analysis + '_matrix.xlsx'
-        Matrix_weights = pd.read_excel(Matrix_weights_name, sheet_name=session_enc)
+        Matrix_results_name = root +  CONDITION + '/' + SUBJECT_USE_ANALYSIS + '_' + algorithm + '_'  + CONDITION + '_'  + distance + '_' + Method_analysis + '.xlsx'
+        Matrix_results = pd.read_excel(Matrix_results_name, sheet_name=0)
