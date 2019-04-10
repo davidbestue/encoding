615e7660ac1c1b23bcb056fd16333e4bd19b3494
davidbestue <davidsanchezbestue@hotmail.com>
2019-04-10 11:47:37 +0200

    Update solve_negative.py

--- scripts/wm_representation/solve_negative.py
+++ scripts/wm_representation/solve_negative.py
@@ -34,8 +34,8 @@ elif platform == "cluster":
 ##
 for SUBJECT_USE_ANALYSIS in ['n001']: #'d001', 'n001', 'r001', 'b001', 'l001', 's001'
     print(SUBJECT_USE_ANALYSIS)
-    for brain_region in ["visual"]:  #"ips"
-        for CONDITION in ['1_0.2']: #, '1_7', '2_0.2', '2_7'
+    for brain_region in ["visual", "ips"]:  #"ips"
+        for CONDITION in ['1_0.2', '1_7', '2_0.2', '2_7']: #, '1_7', '2_0.2', '2_7'
             Method_analysis = 'together'
             #CONDITION = '1_0.2'
             #brain_region = "visual"
