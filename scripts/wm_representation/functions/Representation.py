0f74f776ca20bd7f7bea1e0b614749d75ece3426
davidbestue <davidsanchezbestue@hotmail.com>
2019-04-24 17:06:04 +0200

    update

--- scripts/wm_representation/functions/Representation.py
+++ scripts/wm_representation/functions/Representation.py
@@ -10,16 +10,19 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from basic_functions import *
+from numpy.linalg import inv
+
 
 def Representation(Weights, testing_data, testing_angles):   
-    ref_angle=45
+    Weights_t=Weights.transpose()
+    ref_angle=180
     Channel_all_trials_rolled=[] #Lists to append all the trials rolled
     for trial in range(len(testing_angles)):
         Signal = testing_data[trial, :]
-        channel_36 = np.dot( np.dot ( np.inv( np.dot(Matrix_weights_transpose, Matrix_weights ) ),  Matrix_weights_transpose),  Signal) #Run the inverse model
+        channel_36 = np.dot( np.dot ( inv( np.dot(Weights_t, Weights ) ),  Weights_t),  Signal) #Run the inverse model
         channel= ch2vrep3(channel_36) ###Convert 36 into 720 channels for the reconstruction
         #Roll
-        angle_trial =  testing_angles[angle]
+        angle_trial =  testing_angles[trial]
         to_roll = int( (ref_angle - angle_trial)*(len(channel)/360) ) ## degrees to roll
         channel=np.roll(channel, to_roll) ## roll this degrees
         Channel_all_trials_rolled.append(channel)
@@ -28,14 +31,13 @@ def Representation(Weights, testing_data, testing_angles):
     Channel_all_trials_rolled = np.array(Channel_all_trials_rolled)  # (trials, TRs, channels_activity) (of the session (whne together, all))
     df = pd.DataFrame()
     n = list(Channel_all_trials_rolled.mean(axis=0)) #mean of all the trials rolled
-    df['time_x'] = n #name of the column
-    
-    # plot heatmap
+    df['time_x'] = n #Name of the column
+    #Plot heatmap
     plt.figure()
     plt.title('Heatmap decoding')
     #######midpoint = df.values.mean() # (df.values.max() - df.values.min()) / 2
     ax = sns.heatmap(df, yticklabels=list(df.index), cmap="coolwarm") # cmap= viridis "jet",  "coolwarm" RdBu_r, gnuplot, YlOrRd, CMRmap  , center = midpoint
-    ax.plot([0.25, np.shape(df)[1]-0.25], [posch1_to_posch2(4),posch1_to_posch2(4)], 'k--')
+    ax.plot([0.25, np.shape(df)[1]-0.25], [posch1_to_posch2(18),posch1_to_posch2(18)], 'k--')
     plt.yticks([posch1_to_posch2(4), posch1_to_posch2(13), posch1_to_posch2(22), posch1_to_posch2(31)] ,['45','135','225', '315'])
     plt.ylabel('Angle')
     plt.xlabel('time (s)')
--- scripts/wm_representation/functions/example.py
+++ scripts/wm_representation/functions/example.py
@@ -8,18 +8,25 @@ Created on Wed Apr 24 14:32:56 2019
 from basic_functions import *
 from fake_data_generator import *
 from Weigths_matrix import *
+from Representation import *
 import random
 
 
-n_trials_train=300
-training_angles = np.array([ random.randint(0,359) for i in range(n_trials)])
+n_trials_train=450
+training_angles = np.array([ random.randint(0,359) for i in range(n_trials_train)])
 training_data = fake_data(training_angles)
 
 ##
 WM = Weights_matrix( training_data, training_angles )
 ##
 
-
-n_trials_test=150
+n_trials_test=621
 testing_angles = np.array([ random.randint(0,359) for i in range(n_trials_test)])
 testing_data = fake_data(testing_angles)
+
+Representation(WM, testing_data, testing_angles)
+
+
+
+
+
