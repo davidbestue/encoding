6b3beb0d91e1ef505def3ae94c657eb453c477f0
davidbestue <davidsanchezbestue@hotmail.com>
2019-04-24 14:05:14 +0200

    Update Weigths_matrix.py

--- scripts/wm_representation/functions/Weigths_matrix.py
+++ scripts/wm_representation/functions/Weigths_matrix.py
@@ -5,16 +5,71 @@ Created on Wed Apr 24 13:14:46 2019
 @author: David Bestue
 """
 
+
+import numpy as np
+import pandas as pd
+
 #### function to get Weight matrix
 
 ## trainiing data (trials, vx)
 ## training_angles (trials)
 
 
+#Generate the positions of the channels (there will be 14)
+sep_channels=10
+adjusted_size_contant = 48.519
+
+pos_channels = np.arange(sep_channels/2,360,sep_channels)
+pos_channels = [round(pos_channels[i],3) for i in range(0, len(pos_channels))]
+
+
+def circ_dist(a1,a2):
+    ## Returns the minimal distance in angles between to angles 
+    op1=abs(a2-a1)
+    angs=[a1,a2]
+    op2=min(angs)+(360-max(angs))
+    options=[op1,op2]
+    return min(options)
+
+
+
+def f(position_target):
+    #I want to return a list of the activity of each channel in front of a stim at any location
+    #
+    #The f function imput is the distance from the position to the channel. That is why first we need to
+    #get a distance from the locaion to each channel
+    #
+    #First i calculate the distance in degrees from the location to each channel
+    #Once I have the distance value, I use the same formula as Sprague to extract a value of f for each channel.
+    #colculate the r : the circular distance between target position and each channel
+    list_r=[]
+    for channel in pos_channels:
+        R = round(circ_dist(position_target, channel), 3)
+        list_r.append(R)
+    
+    #I need the adjusted because the r is not in visual angles, it is in degrees
+    #I calculate the f for those inside the spread of the maximum, farther, it is 0
+    f_list=[]
+    for r in list_r:
+        if r<adjusted_size_contant:
+            #f = ( 0.5 + 0.5*cos(r*pi/adjusted_size_contant) )
+            f = ( 0.5 + 0.5*np.cos(r*pi/adjusted_size_contant) )**7
+            f=round(f,3)
+            f_list.append(f)
+        else:
+            f = 0
+            f_list.append(f)
+    
+    
+    #Return the list   
+    return f_list
+
+
+
 
 def Weights_matrix( training_data, training_angles ):
     #####
-    n_voxels = shape(training_data)[0]
+    n_voxels = np.shape(training_data)[0]
     
     ### Expected activity from the model
     M_model=[] #matrix of the activity from the model
@@ -22,13 +77,13 @@ def Weights_matrix( training_data, training_angles ):
         channel_values=f(i)  #f #f_quadrant (function that generates the expectd reponse in each channel)
         M_model.append(channel_values)
         
-    M_model=pd.DataFrame(array(M_model)) # (trials, channel_activity)
+    M_model=pd.DataFrame(np.array(M_model)) # (trials, channel_activity)
     channel_names = ['ch_' +str(i+1) for i in range(0, len(pos_channels))] #names of the channels 
     M_model.columns=channel_names
     
     
     ####   2. Train the model and get matrix of weights
-    Matrix_weights=zeros(( n_voxels, len(pos_channels) )) # (voxels, channels) how each channels is represented in each voxel
+    Matrix_weights=np.zeros(( n_voxels, len(pos_channels) )) # (voxels, channels) how each channels is represented in each voxel
     
     for voxel_x in range(0, n_voxels): #train each voxel
         # set Y and X for the GLM
