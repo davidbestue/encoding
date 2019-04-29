6c9c8396ff90fcbc59ff22a925516b5f5715227d
davidbestue <davidsanchezbestue@hotmail.com>
2019-04-29 17:31:42 +0200

    Update example.py

--- scripts/wm_representation/functions/example.py
+++ scripts/wm_representation/functions/example.py
@@ -12,62 +12,13 @@ from Representation import *
 from process_encoding import *
 from process_wm import *
 from data_to_use import *
-import random
 from joblib import Parallel, delayed
 import multiprocessing
 import time
 
-#
-numcores = multiprocessing.cpu_count()
-#
-#n_trials_train=900
-#training_angles = np.array([ random.randint(0,359) for i in range(n_trials_train)])
-#training_data = fake_data(training_angles)
-#
-###
-#WM = Weights_matrix( training_data, training_angles )
-#WM_t = WM.transpose()
-###
-#
-#n_trials_test=20000
-#testing_angles = np.array([ random.randint(0,359) for i in range(n_trials_test)])
-#testing_data = fake_data(testing_angles)
-#
-#Representation(testing_data, testing_angles, WM, WM_t, ref_angle=180, plot=True)
-
-
-
-###############################################
-###############################################
-###############################################
-###############################################
-###############################################
-
 
 ### Data to use
-root= '/home/david/Desktop/IEM_data/'
-
-masks = [ root+'temp_masks/n001/visual_fsign_rh.nii.gz', root+ 'temp_masks/n001/visual_fsign_lh.nii.gz']
-
-enc_fmri_paths= [root +'n001/encoding/s01/r01/nocfmri3_Encoding_Ax.nii', root +'n001/encoding/s01/r02/nocfmri3_Encoding_Ax.nii', root +'n001/encoding/s01/r03/nocfmri3_Encoding_Ax.nii', root +'n001/encoding/s01/r04/nocfmri3_Encoding_Ax.nii', root +'n001/encoding/s01/r05/nocfmri3_Encoding_Ax.nii',
-             root +'n001/encoding/s02/r01/nocfmri3_Encoding_Ax.nii', root +'n001/encoding/s02/r02/nocfmri3_Encoding_Ax.nii', root +'n001/encoding/s02/r03/nocfmri3_Encoding_Ax.nii', root +'n001/encoding/s02/r04/nocfmri3_Encoding_Ax.nii',
-             root +'n001/encoding/s03/r01/nocfmri3_Encoding_Ax.nii', root +'n001/encoding/s03/r02/nocfmri3_Encoding_Ax.nii', root +'n001/encoding/s03/r03/nocfmri3_Encoding_Ax.nii', root +'n001/encoding/s03/r04/nocfmri3_Encoding_Ax.nii']
-
-enc_beh_paths =[root +'n001/encoding/s01/r01/enc_beh.txt', root +'n001/encoding/s01/r02/enc_beh.txt', root +'n001/encoding/s01/r03/enc_beh.txt', root +'n001/encoding/s01/r04/enc_beh.txt', root +'n001/encoding/s01/r05/enc_beh.txt',
-            root +'n001/encoding/s02/r01/enc_beh.txt', root +'n001/encoding/s02/r02/enc_beh.txt', root +'n001/encoding/s02/r03/enc_beh.txt', root +'n001/encoding/s02/r04/enc_beh.txt',
-            root +'n001/encoding/s03/r01/enc_beh.txt', root +'n001/encoding/s03/r02/enc_beh.txt', root +'n001/encoding/s03/r03/enc_beh.txt', root +'n001/encoding/s03/r04/enc_beh.txt']
-
-
-wm_fmri_paths = [root +'n001/WMtask/s01/r01/nocfmri5_task_Ax.nii', root +'n001/WMtask/s01/r02/nocfmri5_task_Ax.nii', root +'n001/WMtask/s01/r03/nocfmri5_task_Ax.nii', root +'n001/WMtask/s01/r04/nocfmri5_task_Ax.nii', root +'n001/WMtask/s01/r05/nocfmri5_task_Ax.nii',
-                 root +'n001/WMtask/s02/r01/nocfmri5_task_Ax.nii', root +'n001/WMtask/s02/r02/nocfmri5_task_Ax.nii', root +'n001/WMtask/s02/r03/nocfmri5_task_Ax.nii', root +'n001/WMtask/s02/r04/nocfmri5_task_Ax.nii',
-                 root +'n001/WMtask/s03/r01/nocfmri5_task_Ax.nii', root +'n001/WMtask/s03/r02/nocfmri5_task_Ax.nii', root +'n001/WMtask/s03/r03/nocfmri5_task_Ax.nii', root +'n001/WMtask/s03/r04/nocfmri5_task_Ax.nii', root +'n001/WMtask/s03/r05/nocfmri5_task_Ax.nii']
-
-
-wm_beh_paths=[root +'n001/WMtask/s01/r01/wm_beh.txt', root +'n001/WMtask/s01/r02/wm_beh.txt', root +'n001/WMtask/s01/r03/wm_beh.txt', root +'n001/WMtask/s01/r04/wm_beh.txt', root +'n001/WMtask/s01/r05/wm_beh.txt',
-              root +'n001/WMtask/s02/r01/wm_beh.txt', root +'n001/WMtask/s02/r02/wm_beh.txt', root +'n001/WMtask/s02/r03/wm_beh.txt', root +'n001/WMtask/s02/r04/wm_beh.txt',
-              root +'n001/WMtask/s03/r01/wm_beh.txt', root +'n001/WMtask/s03/r02/wm_beh.txt', root +'n001/WMtask/s03/r03/wm_beh.txt', root +'n001/WMtask/s03/r04/wm_beh.txt', root +'n001/WMtask/s03/r05/wm_beh.txt']
-
-
+enc_fmri_paths, enc_beh_paths, wm_fmri_paths, wm_beh_paths, masks = data_to_use( 'n001', 'together', 'ips')
 
 ##### Process training data
 training_dataset, training_targets = process_encoding_files(enc_fmri_paths, masks, enc_beh_paths, sys_use='unix', hd=6, TR=2.335)
@@ -77,7 +28,7 @@ WM = Weights_matrix( training_dataset, training_targets )
 WM_t = WM.transpose()
 
 ##### Process testing data
-testing_activity, testing_behaviour = process_wm_files(wm_fmri_paths, masks, wm_beh_paths, condition='2_7', distance='mix', sys_use='unix', nscans_wm=nscans_wm, TR=2.335)
+testing_activity, testing_behaviour = process_wm_files(wm_fmri_paths, masks, wm_beh_paths, condition='1_0.2', distance='mix', sys_use='unix', nscans_wm=nscans_wm, TR=2.335)
 testing_angles = np.array(testing_behaviour['T'])
 
 
@@ -115,5 +66,34 @@ print( 'Time process reconstruction: ' +str(process_recons))
 
 
 
+###############################################
+###############################################
+###############################################
+###############################################
+###############################################
 
+#import random
+
+#n_trials_train=900
+#training_angles = np.array([ random.randint(0,359) for i in range(n_trials_train)])
+#training_data = fake_data(training_angles)
+#
+###
+#WM = Weights_matrix( training_data, training_angles )
+#WM_t = WM.transpose()
+###
+#
+#n_trials_test=20000
+#testing_angles = np.array([ random.randint(0,359) for i in range(n_trials_test)])
+#testing_data = fake_data(testing_angles)
+#
+#Representation(testing_data, testing_angles, WM, WM_t, ref_angle=180, plot=True)
+
+
+
+###############################################
+###############################################
+###############################################
+###############################################
+###############################################
 
