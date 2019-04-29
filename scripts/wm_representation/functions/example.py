5c45396a9a3510062984219c312ff711da9d1143
davidbestue <davidsanchezbestue@hotmail.com>
2019-04-29 17:26:17 +0200

    update

--- scripts/wm_representation/functions/example.py
+++ scripts/wm_representation/functions/example.py
@@ -11,6 +11,7 @@ from Weigths_matrix import *
 from Representation import *
 from process_encoding import *
 from process_wm import *
+from data_to_use import *
 import random
 from joblib import Parallel, delayed
 import multiprocessing
