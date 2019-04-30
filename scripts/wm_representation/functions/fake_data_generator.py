8688e5b7603ea4c6e0ee16c60fc023086efa7d29
davidbestue <davidsanchezbestue@hotmail.com>
2019-04-30 18:19:45 +0200

    Update fake_data_generator.py

--- scripts/wm_representation/functions/fake_data_generator.py
+++ scripts/wm_representation/functions/fake_data_generator.py
@@ -29,21 +29,21 @@ def fake_data(angles):
 
 
 
-def fake_data_3(T, NT1, NT2):
-    #n_vox=100   
-    # input list of angles
-    data=[]   
-    
-    for angle in range(len(angles)):
-        ang_rad = np.radians(angles[angle]) 
-        #
-        vxls_1_30 = [np.cos(ang_rad) + np.random.normal(0, 1)  for i in range(30)] #30 with cos signal
-        vxls_30_50 = [np.sin(ang_rad) + np.random.normal(0, 1)  for i in range(20)] #20 with sin signal
-        vxls_50_100 = [np.random.normal(0, 1)  for i in range(50)] #50 with noise
-        #
-        vxls_1_100 = vxls_1_30 + vxls_30_50 + vxls_50_100
-        data.append( vxls_1_100 )
-    
-    data_r=np.array(data)
-    return data_r
+#def fake_data_3(T, NT1, NT2):
+#    #n_vox=100   
+#    # input list of angles
+#    data=[]   
+#    
+#    for angle in range(len(angles)):
+#        ang_rad = np.radians(angles[angle]) 
+#        #
+#        vxls_1_30 = [np.cos(ang_rad) + np.random.normal(0, 1)  for i in range(30)] #30 with cos signal
+#        vxls_30_50 = [np.sin(ang_rad) + np.random.normal(0, 1)  for i in range(20)] #20 with sin signal
+#        vxls_50_100 = [np.random.normal(0, 1)  for i in range(50)] #50 with noise
+#        #
+#        vxls_1_100 = vxls_1_30 + vxls_30_50 + vxls_50_100
+#        data.append( vxls_1_100 )
+#    
+#    data_r=np.array(data)
+#    return data_r
     
