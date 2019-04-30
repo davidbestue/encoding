cca1090be60ad8f8adc2d669ea18a43dff602e91
davidbestue <davidsanchezbestue@hotmail.com>
2019-04-30 10:42:23 +0200

    Update example.py

--- scripts/wm_representation/functions/example.py
+++ scripts/wm_representation/functions/example.py
@@ -19,7 +19,7 @@ import random
 
 
 ### Data to use
-enc_fmri_paths, enc_beh_paths, wm_fmri_paths, wm_beh_paths, masks = data_to_use( 'n001', 'together', 'ips')
+enc_fmri_paths, enc_beh_paths, wm_fmri_paths, wm_beh_paths, masks = data_to_use( 'n001', 'together', 'visual')
 
 ##### Process training data
 training_dataset, training_targets = process_encoding_files(enc_fmri_paths, masks, enc_beh_paths, sys_use='unix', hd=4, TR=2.335)
