b5cd18a92e9e64fcac3723086ecb2e803a2b213c
davidbestue <davidsanchezbestue@hotmail.com>
2019-05-06 14:35:56 +0200

    Update fake_data_generator.py

--- scripts/wm_representation/functions/fake_data_generator.py
+++ scripts/wm_representation/functions/fake_data_generator.py
@@ -16,9 +16,9 @@ def fake_data(angles):
     for angle in range(len(angles)):
         ang_rad = np.radians(angles[angle]) 
         #
-        vxls_1_30 = [np.cos(ang_rad) + np.random.normal(0, 1)  for i in range(30)] #30 with cos signal
-        vxls_30_50 = [np.sin(ang_rad) + np.random.normal(0, 1)  for i in range(20)] #20 with sin signal
-        vxls_50_100 = [np.random.normal(0, 1)  for i in range(50)] #50 with noise
+        vxls_1_30 = [np.cos(ang_rad) + np.random.normal(0, 0.001)  for i in range(30)] #30 with cos signal
+        vxls_30_50 = [np.sin(ang_rad) + np.random.normal(0, 0.001)  for i in range(20)] #20 with sin signal
+        vxls_50_100 = [np.random.normal(0, 0.001)  for i in range(50)] #50 with noise
         #
         vxls_1_100 = vxls_1_30 + vxls_30_50 + vxls_50_100
         data.append( vxls_1_100 )
@@ -30,22 +30,25 @@ def fake_data(angles):
 
 
 
-
-#def fake_data_3(T, NT1, NT2):
-#    #n_vox=100   
-#    # input list of angles
-#    data=[]   
-#    
-#    for angle in range(len(angles)):
-#        ang_rad = np.radians(angles[angle]) 
-#        #
-#        vxls_1_30 = [np.cos(ang_rad) + np.random.normal(0, 1)  for i in range(30)] #30 with cos signal
-#        vxls_30_50 = [np.sin(ang_rad) + np.random.normal(0, 1)  for i in range(20)] #20 with sin signal
-#        vxls_50_100 = [np.random.normal(0, 1)  for i in range(50)] #50 with noise
-#        #
-#        vxls_1_100 = vxls_1_30 + vxls_30_50 + vxls_50_100
-#        data.append( vxls_1_100 )
-#    
-#    data_r=np.array(data)
-#    return data_r
+def fake_data_3(T, NT1, NT2):
+    #n_vox=100   
+    # input list of angles
+    data_3=[]
+    data_3.append(fake_data(T))
+    data_3.append(fake_data(NT1))
+    data_3.append(fake_data(NT2))
     
+    data=[]
+    for i in range(len(T)):
+        data.append(  data_3[0][i] +  data_3[1][i] +  data_3[2][i] )
+    
+    
+    data_r=np.array(data)
+    return data_r
+
+
+
+#t=[20, 100]
+#nt1=[320, 32]
+#nt2=[150, 350]
+#fake_data_3(t, nt1, nt2)
