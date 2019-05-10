a8a245e8ae77bbbf2eb8e36ea4b73ca08e9b7d15
davidbestue <davidsanchezbestue@hotmail.com>
2019-05-10 10:50:54 +0200

    Update pipeline_example.py

--- scripts/wm_representation/functions/pipeline_example.py
+++ scripts/wm_representation/functions/pipeline_example.py
@@ -27,40 +27,40 @@ training_dataset, training_targets = process_encoding_files(enc_fmri_paths, mask
 WM = Weights_matrix( training_dataset, training_targets )
 WM_t = WM.transpose()
 
-##### Process testing data
-testing_activity, testing_behaviour = process_wm_files(wm_fmri_paths, masks, wm_beh_paths, condition='1_0.2', distance='mix', sys_use='unix', nscans_wm=nscans_wm, TR=2.335)
-testing_angles = np.array(testing_behaviour['T'])
-#random.shuffle(testing_angles)
-
-### Respresentation
-start_repres = time.time()
-numcores = multiprocessing.cpu_count()
-
-# TR separartion
-signal_paralel =[ testing_activity[:, i, :] for i in range(nscans_wm)]
-Reconstructions = Parallel(n_jobs = numcores)(delayed(Representation)(signal, testing_angles, WM, WM_t, ref_angle=180, plot=False)  for signal in signal_paralel)    ####
-Reconstruction = pd.concat(Reconstructions, axis=1) 
-Reconstruction.columns =  [str(i * TR) for i in range(nscans_wm)]
-
-#Plot heatmap
-plt.figure()
-plt.title('Heatmap decoding')
-######midpoint = df.values.mean() # (df.values.max() - df.values.min()) / 2
-ax = sns.heatmap(Reconstruction, yticklabels=list(Reconstruction.index), cmap="coolwarm") # cmap= viridis "jet",  "coolwarm" RdBu_r, gnuplot, YlOrRd, CMRmap  , center = midpoint
-ax.plot([0.25, np.shape(Reconstruction)[1]-0.25], [posch1_to_posch2(18),posch1_to_posch2(18)], 'k--')
-plt.yticks([posch1_to_posch2(4), posch1_to_posch2(13), posch1_to_posch2(22), posch1_to_posch2(31)] ,['45','135','225', '315'])
-plt.ylabel('Angle')
-plt.xlabel('time (s)')
-plt.tight_layout()
-plt.show(block=False)
-
-######
-######
-######
-
-end_repres = time.time()
-process_recons = end_repres - start_repres
-print( 'Time process reconstruction: ' +str(process_recons))
+###### Process testing data
+#testing_activity, testing_behaviour = process_wm_files(wm_fmri_paths, masks, wm_beh_paths, condition='1_0.2', distance='mix', sys_use='unix', nscans_wm=nscans_wm, TR=2.335)
+#testing_angles = np.array(testing_behaviour['T'])
+##random.shuffle(testing_angles)
+#
+#### Respresentation
+#start_repres = time.time()
+#numcores = multiprocessing.cpu_count()
+#
+## TR separartion
+#signal_paralel =[ testing_activity[:, i, :] for i in range(nscans_wm)]
+#Reconstructions = Parallel(n_jobs = numcores)(delayed(Representation)(signal, testing_angles, WM, WM_t, ref_angle=180, plot=False)  for signal in signal_paralel)    ####
+#Reconstruction = pd.concat(Reconstructions, axis=1) 
+#Reconstruction.columns =  [str(i * TR) for i in range(nscans_wm)]
+#
+##Plot heatmap
+#plt.figure()
+#plt.title('Heatmap decoding')
+#######midpoint = df.values.mean() # (df.values.max() - df.values.min()) / 2
+#ax = sns.heatmap(Reconstruction, yticklabels=list(Reconstruction.index), cmap="coolwarm") # cmap= viridis "jet",  "coolwarm" RdBu_r, gnuplot, YlOrRd, CMRmap  , center = midpoint
+#ax.plot([0.25, np.shape(Reconstruction)[1]-0.25], [posch1_to_posch2(18),posch1_to_posch2(18)], 'k--')
+#plt.yticks([posch1_to_posch2(4), posch1_to_posch2(13), posch1_to_posch2(22), posch1_to_posch2(31)] ,['45','135','225', '315'])
+#plt.ylabel('Angle')
+#plt.xlabel('time (s)')
+#plt.tight_layout()
+#plt.show(block=False)
+#
+#######
+#######
+#######
+#
+#end_repres = time.time()
+#process_recons = end_repres - start_repres
+#print( 'Time process reconstruction: ' +str(process_recons))
 
 
 
