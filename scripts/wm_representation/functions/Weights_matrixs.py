86d0f9f656909011ec972e40ee29d8aae18ade6a
davidbestue <davidsanchezbestue@hotmail.com>
2019-05-15 18:06:10 +0200

    update

--- scripts/wm_representation/functions/Weights_matrixs.py
+++ scripts/wm_representation/functions/Weights_matrixs.py
@@ -275,50 +275,8 @@ def Weights_matrix_Lasso_p( training_data, training_angles ):
 
 
 
-def Weights_matrix_Lasso_neg( training_data, training_angles ):
-    #####
-    #### yes intercept
-    #### yes normalized
-    ### also negative
-    start_train_weights = time.time()
-    #####
-    n_voxels = np.shape(training_data)[1]
-    
-    ### Expected activity from the model
-    M_model=[] #matrix of the activity from the model
-    for i in training_angles:
-        channel_values=f(i)  #f #f_quadrant (function that generates the expectd reponse in each channel)
-        M_model.append(channel_values)
-        
-    M_model=pd.DataFrame(np.array(M_model)) # (trials, channel_activity)
-    channel_names = ['ch_' +str(i+1) for i in range(0, len(pos_channels))] #names of the channels 
-    M_model.columns=channel_names
-    
-    ####   2. Train the model and get matrix of weights
-    Matrix_weights=np.zeros(( n_voxels, len(pos_channels) )) # (voxels, channels) how each channels is represented in each voxel
-    for voxel_x in range(0, n_voxels): #train each voxel
-        
-        ### Lasso with penalization of 0.0001 (higher gives all zeros), fiting intercept (around 10 ) and forcing the weights to be positive
-        Y = training_data[:, voxel_x] ## Y is the real activity
-        X = M_model ## X is the hipothetycal activit
-        lin = Lasso(alpha=0.001, precompute=True,  fit_intercept=True,  positive=False, normalize=True, selection='random')   
-        lin.fit(X,Y) # fits the best combination of weights to explain the activity
-        betas = lin.coef_ #ignore the intercept and just get the weights of each channel
-        Matrix_weights[voxel_x, :]=betas #save the 36 weights for each voxel
-    
-    
-    ####    
-    #Save the matrix of weights 
-    Matrix_weights =pd.DataFrame(Matrix_weights) #convert the array to dataframe
-    end_train_weights = time.time()
-    process_train_weights = end_train_weights - start_train_weights
-    print( 'Time train Weights: ' +str(process_train_weights))   
-    
-    return Matrix_weights
 
-
-
-def Weights_matrix_Lasso_b( training_data, training_angles ):
+def Weights_matrix_Lasso_i_b( training_data, training_angles ):
     #####
     #### yes intercept
     #### also negative
@@ -361,7 +319,7 @@ def Weights_matrix_Lasso_b( training_data, training_angles ):
 
 
 
-def Weights_matrix_Lasso( training_data, training_angles ):
+def Weights_matrix_Lasso_b( training_data, training_angles ):
     #####
     #### no intercept
     #### also negative
