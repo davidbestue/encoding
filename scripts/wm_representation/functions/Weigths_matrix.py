ce920351715332a7c86c993c82e33e31b49dfbe5
davidbestue <davidsanchezbestue@hotmail.com>
2019-05-15 14:45:16 +0200

    different weights stimation

--- scripts/wm_representation/functions/Weigths_matrix.py
+++ scripts/wm_representation/functions/Weigths_matrix.py
@@ -15,11 +15,55 @@ from scipy.stats import zscore
 
 
 
-def Weights_matrix_LM_i_zs( training_data, training_angles ):
+
+
+def Weights_matrix_LM( training_data, training_angles ):
+    # no intercept
+    # no regressors scaling
     #####
-    ## function to get Weight matrix
-    ## trainiing data (trials, vx)
-    ## training_angles (trials)
+    start_train_weights = time.time()
+    #####
+    n_voxels = np.shape(training_data)[1]
+    
+    ### Expected activity from the model
+    M_model=[] #matrix of the activity from the model
+    for i in training_angles:
+        channel_values=f(i)  #f #f_quadrant (function that generates the expectd reponse in each channel)
+        M_model.append(channel_values)
+        
+    M_model=pd.DataFrame(np.array(M_model)) # (trials, channel_activity)
+    channel_names = ['ch_' +str(i+1) for i in range(0, len(pos_channels))] #names of the channels 
+    M_model.columns=channel_names
+    
+    ####   2. Train the model and get matrix of weights
+    Matrix_weights=np.zeros(( n_voxels, len(pos_channels) )) # (voxels, channels) how each channels is represented in each voxel
+    
+    for voxel_x in range(0, n_voxels): #train each voxel
+        # set Y and X for the GLM
+        Y = training_data[:, voxel_x] ## Y is the real activity
+        X = M_model #_zscored #M_model ## X is the hipothetycal activity 
+        ##
+        a = sm.OLS(Y, X )
+        resul = a.fit()
+        betas= resul.params
+        Matrix_weights[voxel_x, :]=betas
+    
+    #Save the matrix of weights 
+    Matrix_weights =pd.DataFrame(Matrix_weights) #convert the array to dataframe
+    end_train_weights = time.time()
+    process_train_weights = end_train_weights - start_train_weights
+    print( 'Time train Weights: ' +str(process_train_weights))   
+    
+    return Matrix_weights
+
+
+
+
+
+def Weights_matrix_LM_i_zs( training_data, training_angles ):
+    ## you fit an intercept
+    ## regressor are scaled
+    #####
     start_train_weights = time.time()
     #####
     n_voxels = np.shape(training_data)[1]
@@ -45,20 +89,8 @@ def Weights_matrix_LM_i_zs( training_data, training_angles ):
         X = sm.add_constant(X)
         a = sm.OLS(Y, X )
         resul = a.fit()
-        #print(resul.params[0])
         betas= resul.params[1:]
         Matrix_weights[voxel_x, :]=betas
-        
-        #### Lasso with penalization of 0.0001 (higher gives all zeros), fiting intercept (around 10 ) and forcing the weights to be positive
-        #Y = training_data[:, voxel_x] ## Y is the real activity
-        #X = M_model## X is the hipothetycal activity (no seed to standarize, normalize of lasso does it)
-        #lin = Lasso(alpha=0.001, precompute=True,  fit_intercept=True,  positive=True, normalize=True, selection='random')   
-        #lin.fit(X,Y) # fits the best combination of weights to explain the activity
-        #betas = lin.coef_ #ignore the intercept and just get the weights of each channel
-        #Matrix_weights[voxel_x, :]=betas #save the 36 weights for each voxel
-    
-    
-    #####
     
     #Save the matrix of weights 
     Matrix_weights =pd.DataFrame(Matrix_weights) #convert the array to dataframe
@@ -73,11 +105,8 @@ def Weights_matrix_LM_i_zs( training_data, training_angles ):
 
 
 
-def Weights_matrix_LM( training_data, training_angles ):
+def Weights_matrix_Lasso( training_data, training_angles ):
     #####
-    ## function to get Weight matrix
-    ## trainiing data (trials, vx)
-    ## training_angles (trials)
     start_train_weights = time.time()
     #####
     n_voxels = np.shape(training_data)[1]
@@ -94,31 +123,18 @@ def Weights_matrix_LM( training_data, training_angles ):
     
     ####   2. Train the model and get matrix of weights
     Matrix_weights=np.zeros(( n_voxels, len(pos_channels) )) # (voxels, channels) how each channels is represented in each voxel
-    #M_model_zscored = zscore(M_model, axis=0) ## standarize if runing the liniar regression
-    
     for voxel_x in range(0, n_voxels): #train each voxel
-        # set Y and X for the GLM
-        Y = training_data[:, voxel_x] ## Y is the real activity
-        X = M_model #_zscored #M_model ## X is the hipothetycal activity 
-        ##
-        #X = sm.add_constant(X)
-        a = sm.OLS(Y, X )
-        resul = a.fit()
-        #print(resul.params[0])
-        betas= resul.params[1:]
-        Matrix_weights[voxel_x, :]=betas
         
-        #### Lasso with penalization of 0.0001 (higher gives all zeros), fiting intercept (around 10 ) and forcing the weights to be positive
-#        Y = training_data[:, voxel_x] ## Y is the real activity
-#        X = M_model ## X is the hipothetycal activit
-#        lin = Lasso(alpha=0.001, precompute=True,  fit_intercept=True,  positive=True, normalize=True, selection='random')   
-#        lin.fit(X,Y) # fits the best combination of weights to explain the activity
-#        betas = lin.coef_ #ignore the intercept and just get the weights of each channel
-#        Matrix_weights[voxel_x, :]=betas #save the 36 weights for each voxel
-#    
+        ### Lasso with penalization of 0.0001 (higher gives all zeros), fiting intercept (around 10 ) and forcing the weights to be positive
+        Y = training_data[:, voxel_x] ## Y is the real activity
+        X = M_model ## X is the hipothetycal activit
+        lin = Lasso(alpha=0.001, precompute=True,  fit_intercept=True,  positive=True, normalize=True, selection='random')   
+        lin.fit(X,Y) # fits the best combination of weights to explain the activity
+        betas = lin.coef_ #ignore the intercept and just get the weights of each channel
+        Matrix_weights[voxel_x, :]=betas #save the 36 weights for each voxel
     
-    #####
     
+    ####    
     #Save the matrix of weights 
     Matrix_weights =pd.DataFrame(Matrix_weights) #convert the array to dataframe
     end_train_weights = time.time()
