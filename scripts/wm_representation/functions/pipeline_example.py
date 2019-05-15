093b213489449a207426217659778a31d4805a41
davidbestue <davidsanchezbestue@hotmail.com>
2019-05-15 16:13:11 +0200

    Update pipeline_example.py

--- scripts/wm_representation/functions/pipeline_example.py
+++ scripts/wm_representation/functions/pipeline_example.py
@@ -20,7 +20,7 @@ import time
 enc_fmri_paths, enc_beh_paths, wm_fmri_paths, wm_beh_paths, masks = data_to_use( 'n001', 'together', 'visual')
 
 ##### Process training data
-training_dataset, training_targets = process_encoding_files(enc_fmri_paths, masks, enc_beh_paths, sys_use='unix', hd=6, TR=2.335)
+training_dataset, training_targets = process_encoding_files(enc_fmri_paths, masks, enc_beh_paths, sys_use='unix', hd=4, TR=2.335)
 
 ##### Train your weigths
 WM = Weights_matrix_LM( training_dataset, training_targets )
