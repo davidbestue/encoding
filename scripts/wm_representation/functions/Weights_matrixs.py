6f247ba4812ecc4e5475b03f5b115eea2d1d92ff
davidbestue <davidsanchezbestue@hotmail.com>
2019-05-20 19:19:13 +0200

    Update Weights_matrixs.py

--- scripts/wm_representation/functions/Weights_matrixs.py
+++ scripts/wm_representation/functions/Weights_matrixs.py
@@ -59,6 +59,47 @@ def Weights_matrix_LM( training_data, training_angles ):
 
 
 
+def Weights_matrix_LM_i( training_data, training_angles ):
+    ## you fit an intercept
+    ## regressor are not scaled
+    #####
+    start_train_weights = time.time()
+    #####
+    n_voxels = np.shape(training_data)[1]
+    
+    ### Expected activity from the model
+    M_model=[] #matrix of the activity from the model
+    for i in training_angles:
+        channel_values=f(i)  #f #f_quadrant (function that generates the expectd reponse in each channel)
+        M_model.append(channel_values)
+        
+    M_model=pd.DataFrame(np.array(M_model)) # (trials, channel_activity)
+    channel_names = ['ch_' +str(i+1) for i in range(0, len(pos_channels))] #names of the channels 
+    M_model.columns=channel_names
+    
+    ####   2. Train the model and get matrix of weights
+    Matrix_weights=np.zeros(( n_voxels, len(pos_channels) +1)) # (voxels, channels) how each channels is represented in each voxel
+    for voxel_x in range(0, n_voxels): #train each voxel
+        # set Y and X for the GLM
+        Y = training_data[:, voxel_x] ## Y is the real activity
+        X = M_model ## X is the hipothetycal activity 
+        ###
+        X = sm.add_constant(X)
+        a = sm.OLS(Y, X )
+        resul = a.fit()
+        betas= resul.params
+        Matrix_weights[voxel_x, :]=betas
+    
+    #Save the matrix of weights 
+    Matrix_weights =pd.DataFrame(Matrix_weights) #convert the array to dataframe
+    end_train_weights = time.time()
+    process_train_weights = end_train_weights - start_train_weights
+    print( 'Time train Weights: ' +str(process_train_weights))    
+    return Matrix_weights
+
+
+
+
 
 def Weights_matrix_LM_i_zs( training_data, training_angles ):
     ## you fit an intercept
@@ -143,45 +184,6 @@ def Weights_matrix_LM_zs( training_data, training_angles ):
 
 
 
-
-
-def Weights_matrix_LM_i( training_data, training_angles ):
-    ## you fit an intercept
-    ## regressor are not scaled
-    #####
-    start_train_weights = time.time()
-    #####
-    n_voxels = np.shape(training_data)[1]
-    
-    ### Expected activity from the model
-    M_model=[] #matrix of the activity from the model
-    for i in training_angles:
-        channel_values=f(i)  #f #f_quadrant (function that generates the expectd reponse in each channel)
-        M_model.append(channel_values)
-        
-    M_model=pd.DataFrame(np.array(M_model)) # (trials, channel_activity)
-    channel_names = ['ch_' +str(i+1) for i in range(0, len(pos_channels))] #names of the channels 
-    M_model.columns=channel_names
-    
-    ####   2. Train the model and get matrix of weights
-    Matrix_weights=np.zeros(( n_voxels, len(pos_channels) +1)) # (voxels, channels) how each channels is represented in each voxel
-    for voxel_x in range(0, n_voxels): #train each voxel
-        # set Y and X for the GLM
-        Y = training_data[:, voxel_x] ## Y is the real activity
-        X = M_model ## X is the hipothetycal activity 
-        ###
-        X = sm.add_constant(X)
-        a = sm.OLS(Y, X )
-        resul = a.fit()
-        betas= resul.params
-        Matrix_weights[voxel_x, :]=betas
-    
-    #Save the matrix of weights 
-    Matrix_weights =pd.DataFrame(Matrix_weights) #convert the array to dataframe
-    end_train_weights = time.time()
-    process_train_weights = end_train_weights - start_train_weights
-    print( 'Time train Weights: ' +str(process_train_weights))    
-    return Matrix_weights
 
 
 
@@ -304,7 +306,7 @@ def Weights_matrix_Lasso_i_b( training_data, training_angles ):
         lin = Lasso(alpha=0.001, precompute=True,  fit_intercept=True,  positive=False, normalize=True, selection='random')   
         lin.fit(X,Y) # fits the best combination of weights to explain the activity
         betas = list( lin.coef_) #ignore the intercept and just get the weights of each channel
-        betas.append( lin.intercept_)
+        betas = [lin.intercept_] + betas
         Matrix_weights[voxel_x, :]=betas #save the 36 weights for each voxel
     
     
