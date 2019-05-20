8bcec2d0afdc8f89988630b9639c0d7278e3d97c
davidbestue <davidsanchezbestue@hotmail.com>
2019-05-20 13:40:58 +0200

    Update process_wm.py

--- scripts/wm_representation/functions/process_wm.py
+++ scripts/wm_representation/functions/process_wm.py
@@ -138,7 +138,7 @@ def wm_timestamps_targets(masked_data, beh_path, n_scans, sys_use='unix', TR=2.3
 ### Each column 
 
 
-def condition_wm( activity, behaviour, condition, distance='mix'):
+def condition_wm( activity, behaviour, condition, distance='mix', zscore_=True):
     if distance=='mix':
         if condition == '1_0.2': 
             Subset = activity[  np.array(behaviour['delay1']==0.2)  *  np.array(behaviour['order']==1) , :, :]
@@ -178,11 +178,12 @@ def condition_wm( activity, behaviour, condition, distance='mix'):
     #####zscore
     ### Per voxel (problem of mixing session, not in encoding)
     ### Get just the scans_wm that I will use
-    n_voxels = np.shape(Subset)[2]
-    nscans_wm = np.shape(Subset)[1]
-    for sc_time in range(nscans_wm):
-        for voxel in range(0, n_voxels):
-            Subset[:, sc_time, voxel] =  np.array( stats.zscore(Subset[:, sc_time, voxel]  ) ) ;
+    if zscore_ == True:
+        n_voxels = np.shape(Subset)[2]
+        nscans_wm = np.shape(Subset)[1]
+        for sc_time in range(nscans_wm):
+            for voxel in range(0, n_voxels):
+                Subset[:, sc_time, voxel] =  np.array( stats.zscore(Subset[:, sc_time, voxel]  ) ) ;
     
     
     ####
@@ -192,7 +193,78 @@ def condition_wm( activity, behaviour, condition, distance='mix'):
 
 ### Example
 #s_act, s_beh = condition_wm( sig, beh, condition='2_7', distance='mix')
+    
 
+
+
+def wm_condition(masked_data, beh_path, n_scans, condition, sys_use='unix', TR=2.335, nscans_wm=16):
+    # Behaviour 
+    beh_path = ub_wind_path(beh_path, system=sys_use) #change depending on windoxs/unix
+    behaviour=np.genfromtxt(beh_path, skip_header=1) #open the file
+    Beh = pd.DataFrame(behaviour)  #convert it to dataframe
+    headers_col = ['type', 'delay1', 'delay2', 'T', 'NT1', 'NT2', 'Dist', 'Dist_NT1', 'Dist_NT2', 'distance_T_dist', 'cue', 'order',
+                'orient', 'horiz_vertical', 'A_R', 'A_err', 'Abs_angle_error', 'Error_interference', 'A_DC', 'A_DC_dist', 'Q_DC', 
+                'A_DF', 'A_DF_dist', 'Q_DF', 'A_DVF', 'Q_DVF', 'A_DVF_dist', 'Q_DVF_dist', 'presentation_att_cue_time', 'presentation_target_time',
+                'presentation_dist_time', 'presentation_probe_time', 'R_T', 'trial_time', 'disp_time']
+    Beh.columns=headers_col #add columns
+    #take off the reference    
+    ref_time = Beh.iloc[-1, 1] # get the reference(diff between tsat¡rt the display and start de recording)
+    start_trial=Beh['presentation_att_cue_time'].iloc[0:-1]  - ref_time #take off the reference  
+    Beh = Beh.iloc[0:-1, :] # behaviour is the same except the last line (reference time) 
+    start_trial_hdf_scans = start_trial/TR#transform seconds to scans 
+    timestamps = [  int(round(  start_trial_hdf_scans[n] ) ) for n in range(0, len(start_trial_hdf_scans) )]
+    
+    #adjust according to the number of scans you want (avoid having an incomplete trial)
+    while timestamps[-1]> (n_scans-nscans_wm):
+        timestamps=timestamps[:-1] #take off one trial form activity
+        Beh = Beh.iloc[0:-1, :] #take off one trial from behaviour
+    
+    
+    #append the timestands you want from this session
+    n_trials = len(timestamps)
+    n_voxels_wm = np.shape(masked_data)[1]
+    ### Take the important TRs (from cue, the next 14 TRs)
+    run_activity=np.zeros(( n_trials, nscans_wm,  n_voxels_wm   )) ## np.zeros matrix with the correct dimensions of the session
+    for idx, t in enumerate(timestamps): #beginning of the trial
+        for sc in range(0, nscans_wm): #each of the 14 TRs
+            trial_activity = masked_data[t+sc, :]   
+            run_activity[idx, sc, :] =trial_activity                    
+    
+    ### 
+    
+    Subset, beh_Subset = condition_wm( run_activity, Beh, condition, distance='mix', zscore_=False)
+    
+    return Subset, beh_Subset
+
+
+
+
+
+def preprocess_wm_files(wm_fmri_paths, masks, wm_beh_paths, condition, distance='mix', sys_use='unix', nscans_wm=16, TR=2.335):
+    ### Mask and process the fmri data
+    start_process_wm = time.time()
+    numcores = multiprocessing.cpu_count()
+    wm_masked= Parallel(n_jobs = numcores)(delayed(mask_fmri_process)(fmri_path, masks, sys_use='unix')  for fmri_path in wm_fmri_paths)    ####
+    scans_wm_runs = [len(wm_masked[r]) for r in range(len(wm_masked)) ]
+    
+    ### TRs of interest
+    activity_beh  = Parallel(n_jobs = numcores)(delayed(wm_condition)(masked_data, beh_path, n_scans, condition, sys_use='unix', TR=TR, nscans_wm=nscans_wm) for masked_data, beh_path, n_scans in zip( wm_masked, wm_beh_paths, scans_wm_runs))    ####
+    runs_signal = [activity_beh[i][0] for i in range(len(activity_beh))]
+    runs_beh = [activity_beh[i][1] for i in range(len(activity_beh))]
+    
+    ## concatenate the runs
+    testing_activity = np.vstack(runs_signal)
+    testing_behaviour = pd.concat(runs_beh)
+    ##
+    end_process_wm = time.time()
+    process_wm = end_process_wm - start_process_wm
+    print( 'Time process wm: ' +str(process_wm))
+    return testing_activity, testing_behaviour
+
+###
+
+
+#testing_activity, testing_behaviour = process_wm_files(wm_fmri_paths, masks, wm_beh_paths, condition='2_7', distance='mix', sys_use='unix', nscans_wm=16, TR=2.335)
 
 def process_wm_files(wm_fmri_paths, masks, wm_beh_paths, condition, distance='mix', sys_use='unix', nscans_wm=16, TR=2.335):
     ### Mask and process the fmri data
@@ -216,12 +288,6 @@ def process_wm_files(wm_fmri_paths, masks, wm_beh_paths, condition, distance='mi
     process_wm = end_process_wm - start_process_wm
     print( 'Time process wm: ' +str(process_wm))
     return testing_activity, testing_behaviour
-
-###
-
-
-#testing_activity, testing_behaviour = process_wm_files(wm_fmri_paths, masks, wm_beh_paths, condition='2_7', distance='mix', sys_use='unix', nscans_wm=16, TR=2.335)
-
 
 
 
