e4cd1a44ee5c1daf32a6932f5eb5fc2bb4fa6ef2
davidbestue <davidsanchezbestue@hotmail.com>
2019-05-27 10:53:27 +0200

    Update signal_noise.py

--- scripts/wm_representation/functions/signal_noise.py
+++ scripts/wm_representation/functions/signal_noise.py
@@ -152,7 +152,7 @@ for condition in ['1_0.2', '1_7', '2_0.2', '2_7']:
     ax2.fill_between(  [ d_p1, d_p2 ], [y_vl_min, y_vl_min], [y_vl_max, y_vl_max], color='g', alpha=0.3, label='distractor'  )
     ax2.fill_between(  [ r_t1, r_t2 ], [y_vl_min, y_vl_min], [y_vl_max, y_vl_max], color='y', alpha=0.3, label='response'  )    
     
-    sns.lineplot(ax= ax3, x="times", y="decoding", hue='label', hue_order = ['signal', 'shuffle'],  data=df.loc[ (df['condition']==condition)  & (df['region'] =='ips')]) 
+    sns.lineplot(ax= ax3, x="times", y="decoding", hue='label', hue_order = ['signal', 'shuffle'],  data=df.loc[ (df['condition']==condition)  & (df['region'] =='pfc')]) 
     ax3.fill_between(  [ t_p1, t_p2 ], [y_vl_min, y_vl_min], [y_vl_max, y_vl_max], color='b', alpha=0.3, label='target'  )
     ax3.fill_between(  [ d_p1, d_p2 ], [y_vl_min, y_vl_min], [y_vl_max, y_vl_max], color='g', alpha=0.3, label='distractor'  )
     ax3.fill_between(  [ r_t1, r_t2 ], [y_vl_min, y_vl_min], [y_vl_max, y_vl_max], color='y', alpha=0.3, label='response'  )  
