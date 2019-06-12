197f5bc8f598cb713e180ef95c38f92829feb1fe
davidbestue <davidsanchezbestue@hotmail.com>
2019-06-12 14:45:50 +0200

    Update signal_noise.py

--- scripts/wm_representation/functions/signal_noise.py
+++ scripts/wm_representation/functions/signal_noise.py
@@ -363,7 +363,7 @@ for brain_region in ['visual', 'ips', 'frontsup', 'frontmid', 'frontinf']: #['vi
 
 df = pd.concat(subj_decoding)
 
-
+fig = plt.figure(figsize=(10,8))
 
 
 for indx_c, condition in enumerate(['1_0.2', '1_7', '2_0.2', '2_7']):
@@ -420,13 +420,14 @@ for indx_c, condition in enumerate(['1_0.2', '1_7', '2_0.2', '2_7']):
     y_vl_max = 5 #◙df_all_by_subj.Decoding.max()
     
     #fig = plt.figure()
-    plt.subplot(2,2, indx_c+1)
+    ax = fig.add_subplot(2,2, indx_c+1) 
+    #plt.subplot(2,2, indx_c+1)
     #ñfig.set_size_inches(20, 20)
     #fig.tight_layout()
     #fig.suptitle(condition)
     #ax1 = fig.add_subplot(111)
     #sns.lineplot(ax=ax1, x="times", y="decoding", hue='region', hue_order =  ['visual', 'ips', 'frontsup', 'frontmid', 'frontinf'],  ci=69, data=df.loc[ (df['condition']==condition)]) 
-    sns.lineplot( x="times", y="decoding", hue='region', hue_order =  ['visual', 'ips', 'frontsup', 'frontmid', 'frontinf'],  ci=69,  data=df.loc[ (df['condition']==condition)]) 
+    sns.lineplot( ax=ax, x="times", y="decoding", hue='region', hue_order =  ['visual', 'ips', 'frontsup', 'frontmid', 'frontinf'],  ci=69,  data=df.loc[ (df['condition']==condition)]) 
     
     plt.plot([0, 35], [0,0], 'k--')
 #    ax1.fill_between(  [ t_p1, t_p2 ], [y_vl_min, y_vl_min], [y_vl_max, y_vl_max], color='b', alpha=0.3, label='target'  )
@@ -465,17 +466,18 @@ for indx_c, condition in enumerate(['1_0.2', '1_7', '2_0.2', '2_7']):
     
     #plt.show(block=False)
 
+
+plt.suptitle( 'LM', fontsize=18)
 plt.tight_layout()
-plt.suptitle( 'Signal-shuffle conditions', fontsize=12)
 plt.show(block=False)
 
 
 
 
-fig = plt.figure()  # create a figure object
+#☼fig = plt.figure()  # create a figure object
 ax = fig.add_subplot(1, 1, 1)  # create an axes object in the figure
 
-fig = plt.figure(figsize=(6,8))
+#fig = plt.figure(figsize=(6,8))
 
 
 #
