598d8466d461927aec1f8588bc3b1f8cc063b890
davidbestue <davidsanchezbestue@hotmail.com>
2019-06-13 11:43:23 +0200

    Update all_process_shuffle.py

--- scripts/wm_representation/functions/all_process_shuffle.py
+++ scripts/wm_representation/functions/all_process_shuffle.py
@@ -113,9 +113,9 @@ def all_process_condition_shuff( Subject, Brain_Region, WM, WM_t, Inter, Conditi
 ##########################################################################################################
 
 
-path_save_reconstructions = '/home/david/Desktop/Reconstructions_LM_n001_2_7_visual.xlsx'
+path_save_reconstructions = '/home/david/Desktop/Reconstructions_LM_n001_2_7_visual_2.xlsx'
 Reconstructions={}
-path_save_shuffle = '/home/david/Desktop/Reconstructions_LM_n001_2_7_visual_suff.xlsx'
+path_save_shuffle = '/home/david/Desktop/Reconstructions_LM_n001_2_7_visual_suff_2.xlsx'
 Reconstructions_shuff=[]
 
 
@@ -136,7 +136,7 @@ for Subject in Subjects:
         WM_t = WM.transpose()
         for idx_c, Condition in enumerate(Conditions):
             plt.subplot(2,2,idx_c+1)
-            Reconstruction, shuff = all_process_condition_shuff( Subject=Subject, Brain_Region=Brain_region, WM=WM, WM_t=WM_t, iterations=100, Inter=Inter, Condition=Condition, method='together',  heatmap=False)
+            Reconstruction, shuff = all_process_condition_shuff( Subject=Subject, Brain_Region=Brain_region, WM=WM, WM_t=WM_t, iterations=2, Inter=Inter, Condition=Condition, method='together',  heatmap=False) #100
             Reconstructions[Subject + '_' + Brain_region + '_' + Condition]=Reconstruction
             Reconstructions_shuff.append(shuff)
             ## Plot the 4 heatmaps
