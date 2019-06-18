456ffe90988c36833d8f2278740733ea9e886fdc
davidbestue <davidsanchezbestue@hotmail.com>
2019-06-18 12:00:52 +0200

    new_file

--- scripts/wm_representation/functions/plt_condit.py
+++ scripts/wm_representation/functions/plt_condit.py
@@ -1,75 +1,25 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jun 13 11:45:04 2019
+Created on Tue Jun 18 11:59:04 2019
 
 @author: David Bestue
 """
+
 
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-from model_functions import *
-
-#path_save_signal ='/home/david/Desktop/signal_LMaxpv_n001.xlsx'
-#path_save_shuffle = '/home/david/Desktop/shuff_LMaxpv_n001.xlsx'
-
-path_save_signal ='/home/david/Desktop/signal_LM.xlsx'
-path_save_shuffle = '/home/david/Desktop/shuff_LM.xlsx'
 
 
-Df = pd.read_excel(path_save_signal) #convert them to pd.dataframes
-Df_shuff = pd.read_excel(path_save_shuffle)
+dfs = pd.read_excel('dfsn.xlsx')
+n = pd.read_excel('n.xlsx')
 
-
-df = pd.concat([Df, Df_shuff]) #concatenate the files
 
 presentation_period= 0.35 #stim presnetation time
 presentation_period_cue=  0.50 #presentation of attentional cue time
 pre_stim_period= 0.5 #time between cue and stim
-resp_time = 4  #time the response is active
-
-
-##### Measure of difference to shuffle
-subj_decoding=[]
-for brain_region in ['visual', 'ips', 'frontsup', 'frontmid', 'frontinf']: #['visual', 'ips', 'pfc']: ['front_sup', 'front_mid', 'front_inf']
-    for condition in ['1_0.2', '1_7', '2_0.2', '2_7']:        
-        for subject in df.subject.unique():
-            decode_timepoint = []
-            for times in df.times.unique():                
-                values = df.loc[(df['label']=='shuffle') & (df['condition']==condition) & (df['region'] ==brain_region)  & (df['subject'] ==subject) & (df['times']==times), 'decoding'] ## all shuffled reconstructions
-                value_decoding = df.loc[(df['label']=='signal') & (df['condition']==condition) & (df['region'] ==brain_region)  & (df['subject'] ==subject) & (df['times']==times), 'decoding'].values[0] #the real reconstruction
-                #### zscore method
-                prediction = ( value_decoding - np.mean(values) )/ np.std(values) #zscore to get the decode compared to shuffle
-                decode_timepoint.append(prediction)
-            ####
-            decode_timepoint = pd.DataFrame(decode_timepoint) 
-            decode_timepoint.columns=['decoding'] #decode compared to shuffle
-            decode_timepoint['times'] = df.times.unique() #times
-            decode_timepoint['subject'] = subject
-            decode_timepoint['region'] = brain_region
-            decode_timepoint['condition'] = condition
-            subj_decoding.append( decode_timepoint)
-
-
-
-dfsn = pd.concat(subj_decoding) #put together for subject, condition and brain region the decoding value compared to shuffle.
-
-### line for decoding shuffled move to 0
-from sklearn import preprocessing
-n = Df_shuff[['decoding', 'times']]
-n = n.reset_index()[['decoding', 'times']]
-n = n.pivot(columns='times')
-x=n.values
-mean_scaler = preprocessing.StandardScaler(with_mean= True, with_std=False) #center but do not remove the std
-x_scaled = mean_scaler.fit_transform(x)
-n = pd.DataFrame(x_scaled)
-n = n.melt()
-n = n[~np.isnan(n.iloc[:,1])]
-n = n.reset_index()[['variable', 'value']]
-n['variable'] = n['variable'].replace( list(range(16)), Df_shuff.times.unique() )
-n.columns=['times', 'decoding']
-
+resp_time = 4  #time the response is activ
 
 
 fig = plt.figure(figsize=(10,8))
@@ -158,11 +108,6 @@ for indx_c, condition in enumerate(['1_0.2', '1_7', '2_0.2', '2_7']):
 plt.suptitle( 'LM', fontsize=18) ## main title
 plt.tight_layout(w_pad=5, h_pad=5, rect=[0, 0.03, 1, 0.95]) #correct the space between graphs
 plt.show(block=False) #show
-
-#plt.figure()
-#sns.lineplot(x='times', y='decoding', data=Df_shuff) #figure to get the intervals of shuffle
-#plt.show(block=False)
-
 
 
 
