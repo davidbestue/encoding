b11e0a81ba2085d294e3b49e066c78232c406573
davidbestue <davidsanchezbestue@hotmail.com>
2019-06-19 12:50:00 +0200

    Update Representation.py

--- scripts/wm_representation/functions/Representation.py
+++ scripts/wm_representation/functions/Representation.py
@@ -64,6 +64,7 @@ def Representation(testing_data, testing_angles, Weights, Weights_t, ref_angle=1
     ###
     numcores = multiprocessing.cpu_count()
     Channel_all_trials_rolled = Parallel(n_jobs = numcores)(delayed(trial_rep)(Signal, angle_trial, Weights, Weights_t, ref=ref_angle, intercept_ = intercept)  for Signal, angle_trial in zip( data_prall, testing_angles))    ####
+    #Channel_all_trials_rolled = Parallel(n_jobs = numcores)(delayed(trial_rep_decode_trial_by_trial)(Signal, angle_trial, Weights, Weights_t, ref=ref_angle, intercept_ = intercept)  for Signal, angle_trial in zip( data_prall, testing_angles))    ####
     Channel_all_trials_rolled = np.array(Channel_all_trials_rolled)
     
     df = pd.DataFrame()
