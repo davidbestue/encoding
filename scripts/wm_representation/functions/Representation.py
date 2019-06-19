0fb37982230c1a3acc83eab7a404009f20b1d1cd
davidbestue <davidsanchezbestue@hotmail.com>
2019-06-19 12:48:04 +0200

    Update Representation.py

--- scripts/wm_representation/functions/Representation.py
+++ scripts/wm_representation/functions/Representation.py
@@ -32,6 +32,25 @@ def trial_rep(Signal, angle_trial, Weights, Weights_t, ref, intercept_):
 
 
 
+
+def trial_rep_decode_trial_by_trial(Signal, angle_trial, Weights, Weights_t, ref, intercept_):
+    ###
+    channel_36 = np.dot( np.dot ( np.linalg.pinv( np.dot(Weights_t, Weights ) ),  Weights_t),  Signal) #Run the inverse model
+    ###
+    if intercept_==True:     
+        channel= ch2vrep3_int(channel_36)
+    else: #no intercept in the Weights matrix
+        channel= ch2vrep3(channel_36) ###Convert 36 into 720 channels for the reconstruction
+    ##
+    channel_dec =  channel * f2(angle_trial)
+    #to_roll = int( (ref - angle_trial)*(len(channel)/360) ) ## degrees to roll
+    #channel=np.roll(channel, to_roll) ## roll this degrees
+    return channel_dec ### in noise you will also have a peak here, the point is that with signal will be higher (reconstruct by substracting noise here)
+
+
+
+
+
 def Representation(testing_data, testing_angles, Weights, Weights_t, ref_angle=180, plot=False, intercept=False):
     ## Make the data parallelizable
     n_trials_test = len(testing_data) #number trials
