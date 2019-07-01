7eaae3451d55eba00f0afe87c3a56ca1fd2089b8
davidbestue <davidsanchezbestue@hotmail.com>
2019-07-01 18:57:07 +0200

    Update bootstrap_reconstruction.py

--- scripts/wm_representation/functions/bootstrap_reconstruction.py
+++ scripts/wm_representation/functions/bootstrap_reconstruction.py
@@ -7,87 +7,42 @@ Created on Mon Jul  1 18:24:32 2019
 
 
 
-
-
-
-def shuffled_reconstruction(signal_paralel, targets, iterations, WM, WM_t, Inter, region, condition, subject, ref_angle=180):
-    ### shuffle the targets
-    testing_angles_sh=[] #new targets shuffled
-    for n_rep in range(iterations):
-        #new_targets = random.sample(targets, len(targets)) #shuffle the labels of the target
-        #testing_angles_sh.append(new_targets)
-        testing_angles_sh.append( np.array([random.choice([0, 90, 180, 270]) for i in range(len(targets))])) ## instead of shuffle, take a region where there is no activity!
-    
-    ### make the reconstryctions and append them
-    Reconstructions_sh=[]
-    for n_rep in range(iterations):
-        time_rec_shuff_start = time.time() #time it takes
-        Reconstructions_i = Parallel(n_jobs = numcores)(delayed(Representation)(signal, testing_angles_sh[n_rep], WM, WM_t, intercept=Inter, ref_angle=180, plot=False)  for signal in signal_paralel) 
-        Reconstruction_i = pd.concat(Reconstructions_i, axis=1) #mean of all the trials
-        Reconstruction_i.columns =  [str(i * TR) for i in range(nscans_wm)] #column names
-        Reconstructions_sh.append(Reconstruction_i) #append the reconstruction (of the current iteration)
-        time_rec_shuff_end = time.time() #time
-        time_rec_shuff = time_rec_shuff_end - time_rec_shuff_start
-        print('shuff_' + str(n_rep) + ': ' +str(time_rec_shuff) ) #print time of the reconstruction shuffled
-    
-    ### Get just the supposed target location
-    df_shuffle=[]
-    for i in range(len(Reconstructions_sh)):
-        n = Reconstructions_sh[i].iloc[ref_angle*2, :] #around the ref_angle (x2 beacuse now we have 720 instead of 360)
-        n = n.reset_index()
-        n.columns = ['times', 'decoding']
-        n['decoding'] = [sum(Reconstructions_sh[i].iloc[:, ts] * f2(ref_angle)) for ts in range(len(n))] #population vector method (scalar product)
-        n['times']=n['times'].astype(float)
-        n['region'] = region
-        n['subject'] = subject
-        n['condition'] = condition
-        df_shuffle.append(n) #save thhis
-    
-    ##
-    df_shuffle = pd.concat(df_shuffle)    #same shape as the decosing of the signal
-    return df_shuffle
-
-
-
-
-
-
-
-
-
-
-def bootstrap_reconstruction(signal_paralel, targets, iterations, WM, WM_t, Inter, region, condition, subject, ref_angle=180):
+def bootstrap_reconstruction(testing_activity, targets, iterations, WM, WM_t, Inter, region, condition, subject, ref_angle=180):
+    Reconstructions_boots=[]
+    signal_paralel =[ testing_activity[:, i, :] for i in range(nscans_wm)]
     ### shuffle the targets
     for n_rep in range(iterations):
+        time_rec_boot_start=time.time()
         indexes_boots = np.random.randint(0,len(targets), len(targets))  #bootstraped indexes for reconstruction
         ### make the reconstryctions and append them
-        Reconstructions_sh=[]
-        for n_rep in range(iterations):
-            time_rec_shuff_start = time.time() #time it takes
-            Reconstructions_i = Parallel(n_jobs = numcores)(delayed(Representation)(signal, testing_angles_sh[n_rep], WM, WM_t, intercept=Inter, ref_angle=180, plot=False)  for signal in signal_paralel) 
-            Reconstruction_i = pd.concat(Reconstructions_i, axis=1) #mean of all the trials
-            Reconstruction_i.columns =  [str(i * TR) for i in range(nscans_wm)] #column names
-            Reconstructions_sh.append(Reconstruction_i) #append the reconstruction (of the current iteration)
-            time_rec_shuff_end = time.time() #time
-            time_rec_shuff = time_rec_shuff_end - time_rec_shuff_start
-            print('shuff_' + str(n_rep) + ': ' +str(time_rec_shuff) ) #print time of the reconstruction shuffled
-    
+        targets_boot = targets[indexes_boots]
+        signal_boots = testing_activity[indexes_boots, :, :] 
+        signal_boots_paralel =[ signal_boots[:, i, :] for i in range(nscans_wm)]
+        
+        Reconstructions_boot = Parallel(n_jobs = numcores)(delayed(Representation)(signal, targets_boot, WM, WM_t, ref_angle=180, plot=False, intercept=Inter)  for signal in signal_boots_paralel)    #### reconstruction standard (paralel)
+        Reconstructions_boot = pd.concat(Reconstructions_boot, axis=1) #mean of all the trials
+        Reconstructions_boot.columns =  [str(i * TR) for i in range(nscans_wm)] #column names
+        Reconstructions_boots.append(Reconstructions_boot) #append the reconstruction (of the current iteration)
+        time_rec_boot_end = time.time() #time
+        time_rec_boot = time_rec_boot_end - time_rec_boot_start
+        print('boot_' + str(n_rep) + ': ' +str(time_rec_boot) ) #print time of the reconstruction shuffled
+        
     ### Get just the supposed target location
-    df_shuffle=[]
-    for i in range(len(Reconstructions_sh)):
-        n = Reconstructions_sh[i].iloc[ref_angle*2, :] #around the ref_angle (x2 beacuse now we have 720 instead of 360)
+    df_boots=[]
+    for i in range(len(Reconstructions_boots)):
+        n = Reconstructions_boots[i].iloc[ref_angle*2, :] #around the ref_angle (x2 beacuse now we have 720 instead of 360)
         n = n.reset_index()
         n.columns = ['times', 'decoding']
-        n['decoding'] = [sum(Reconstructions_sh[i].iloc[:, ts] * f2(ref_angle)) for ts in range(len(n))] #population vector method (scalar product)
+        n['decoding'] = [sum(Reconstructions_boots[i].iloc[:, ts] * f2(ref_angle)) for ts in range(len(n))] #population vector method (scalar product)
         n['times']=n['times'].astype(float)
         n['region'] = region
         n['subject'] = subject
         n['condition'] = condition
-        df_shuffle.append(n) #save thhis
+        df_boots.append(n) #save thhis
     
     ##
-    df_shuffle = pd.concat(df_shuffle)    #same shape as the decosing of the signal
-    return df_shuffle
+    df_boots = pd.concat(df_boots)    #same shape as the decosing of the signal
+    return df_boots
 
 
 
@@ -118,15 +73,13 @@ def all_process_condition_shuff_boot( Subject, Brain_Region, WM, WM_t, Inter, Co
     ######
     ######
     ######
-    boots_signal
-    
-    
     end_repres = time.time()
     process_recons = end_repres - start_repres
     print( 'Time process reconstruction: ' +str(process_recons)) #print time of the process
     
+    df_boots = bootstrap_reconstruction(testing_activity, targets, iterations, WM, WM_t, Inter, region, condition, subject, ref_angle=180)    
     ####### Shuff
     #### Compute the shuffleing
     shuffled_rec = shuffled_reconstruction(signal_paralel, testing_angles, iterations, WM, WM_t, Inter=Inter, region=Brain_Region, condition=Condition, subject=Subject, ref_angle=180)
     
-    return Reconstruction, shuffled_rec
+    return Reconstruction, shuffled_rec, df_boots
