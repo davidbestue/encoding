e835702beb2d2ad73221053cb0d3ddb901875b86
davidbestue <davidsanchezbestue@hotmail.com>
2019-07-31 11:10:49 +0200

    Update decoding_value_boot.py

--- scripts/wm_representation/functions/decoding_value_boot.py
+++ scripts/wm_representation/functions/decoding_value_boot.py
@@ -12,24 +12,30 @@ import seaborn as sns
 import numpy as np
 
 
+
+## for the response (mixed)
 # path_ox = 'C:\\Users\\David\\Desktop\\boots_LM_response_boot_ox.xlsx'
 # path_nit = 'C:\\Users\\David\\Desktop\\boots_LM_response_boot_nit.xlsx'
 # path_hid = 'C:\\Users\\David\\Desktop\\boots_LM_response_boot_hid.xlsx'
 # path_save_shuffle = 'C:\\Users\\David\\Desktop\\shuff_LM.xlsx'
-
-path_ox = 'C:\\Users\\David\\Desktop\\boots_LM_response_boot_ox.xlsx'
-path_nit = 'C:\\Users\\David\\Desktop\\boots_LM_response_boot_nit.xlsx'
-path_hid = 'C:\\Users\\David\\Desktop\\boots_LM_response_boot_hid.xlsx'
-path_save_shuffle = 'C:\\Users\\David\\Desktop\\shuff_LM.xlsx'
+#Df_shuff = pd.read_excel(path_save_shuffle)
 
 
-Df_shuff = pd.read_excel(path_save_shuffle)
+path_ox = 'C:\\Users\\David\\Desktop\\boots_LM_response_boot_far_ox.xlsx'
+path_nit = 'C:\\Users\\David\\Desktop\\boots_LM_response_boot_far_nit.xlsx'
+path_hid = 'C:\\Users\\David\\Desktop\\boots_LM_response_boot_far_hid.xlsx'
+path_ox_shuffle = 'C:\\Users\\David\\Desktop\\shuff_LM_response_boot_far_ox.xlsx'
+path_nit_shuffle = 'C:\\Users\\David\\Desktop\\shuff_LM_response_boot_far_nit.xlsx'
+path_hid_shuffle = 'C:\\Users\\David\\Desktop\\shuff_LM_response_boot_far_hid.xlsx'
+
 
 df_ox = pd.read_excel(path_ox)
 df_nit = pd.read_excel(path_nit)
 df_hid = pd.read_excel(path_hid)
 
-
+shuff_ox = pd.read_excel(path_ox_shuffle)
+shuff_nit = pd.read_excel(path_nit_shuffle)
+shuff_hid = pd.read_excel(path_hid_shuffle)
 
 
 
