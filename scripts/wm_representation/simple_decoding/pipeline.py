b9dd86a82448e9770f64403e04eb7acf03c44f2c
davidbestue <davidsanchezbestue@hotmail.com>
2019-08-07 17:23:52 +0200

    Update pipeline.py

--- scripts/wm_representation/simple_decoding/pipeline.py
+++ scripts/wm_representation/simple_decoding/pipeline.py
@@ -4,10 +4,95 @@ Created on Wed Apr 24 14:32:56 2019
 @author: David Bestue
 """
 
-
+import statsmodels.api as sm
 import sys
 sys.path.append('/home/david/Desktop/GitHub/encoding/scripts/wm_representation/functions/')
 from data_to_use import *
+from process_encoding import *
+
+
+Subject='n001'
+Brain_region='visual'
+
+
+### Data to use
+enc_fmri_paths, enc_beh_paths, wm_fmri_paths, wm_beh_paths, masks = data_to_use( Subject, 'together', Brain_region)
+##### Process training data
+training_dataset, training_targets = process_encoding_files(enc_fmri_paths, masks, enc_beh_paths, sys_use='unix', hd=6, TR=2.335) #4
+##### Train your weigths
+WM, Inter = Weights_matrix_LM( training_dataset, training_targets )
+WM_t = WM.transpose()
+
+
+
+def train_each_vxl( training_dataset, training_targets ):
+	#
+	### X Training
+	## X matrix (intercept and spikes)
+	X = np.column_stack([np.ones(np.shape(training_dataset)[0]),  training_dataset])
+	## Y (sinus and cos of the target)
+	sinus =np.sin([np.radians(np.array(training_targets)[i]) for i in range(0, len(training_targets))])
+	cosinus = np.cos([np.radians(np.array(training_targets)[i]) for i in range(0, len(training_targets))])
+	Y = np.column_stack([cosinus, sinus])
+	Y = Y.astype(float) #to make it work in the cluster
+	X = X.astype(float)
+	model = sm.OLS(Y, X)
+	##train the model
+	fit=model.fit()
+
+
+
+
+
+
+	return weights
+
+
+
+spikes_train, spikes_test, beh_train, beh_test = train_test_split(df.groupby('neuron').get_group(Neur)['spikes'],
+                                                                  df.groupby('neuron').get_group(Neur)['beh'],
+                                                                  test_size=size_test)  
+
+######## Trainning #########
+## X matrix (intercept and spikes)
+X = np.column_stack([np.ones(np.shape(spikes_train)[0]),spikes_train])
+## Y (sinus and cos)
+sinus =np.sin([np.radians(np.array(beh_train)[i]) for i in range(0, len(beh_train))])
+cosinus = np.cos([np.radians(np.array(beh_train)[i]) for i in range(0, len(beh_train))])
+Y = np.column_stack([cosinus, sinus])
+### one OLS for sin and cos: output: beta of intercetp and bea of spikes (two B intercepts and 2 B for spikes )
+Y = Y.astype(float) #to make it work in the cluster
+X = X.astype(float)
+model = sm.OLS(Y, X)
+##train the model
+fit=model.fit()
+
+######### Testing ###########
+X = np.column_stack([np.ones(np.shape(spikes_test)[0]),spikes_test])
+p = fit.predict(X)
+x = p[:,0]
+y = p[:,1]
+#####
+##### Error --> take the resulting vector in sin/cos space
+### from sin and cos get the angle (-pi, pi)
+#pred_angle = [ np.degrees(np.arctan2(y[i], x[i]) + np.pi) for i in range(0, len(y))]
+pred_angle = [ np.degrees(np.arctan2(y[i], x[i])) for i in range(0, len(y))]
+for i in range(0, len(pred_angle)):
+    if pred_angle[i]<0:
+        pred_angle[i]=360+pred_angle[i]
+##
+#error=[ circdist(beh_test[i], pred_angle[i]) for i in range(0, len(pred_angle))]
+error=[ circdist(beh_test.values[i], pred_angle[i]) for i in range(0, len(pred_angle))]
+
+#low_value --> predicted positionns close to real
+neur_err.append(np.mean(error))
+
+
+
+
+
+
+
 
 
 
