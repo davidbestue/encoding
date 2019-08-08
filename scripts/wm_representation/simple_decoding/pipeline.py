c0b2cd50b61c23b23ad37cbd41e28c1433a2965a
davidbestue <davidsanchezbestue@hotmail.com>
2019-08-08 15:23:59 +0200

    Update pipeline.py

--- scripts/wm_representation/simple_decoding/pipeline.py
+++ scripts/wm_representation/simple_decoding/pipeline.py
@@ -97,9 +97,131 @@ for Subject in ['n001', 'd001', 'r001', 's001', 'b001', 'l001']:
 
 
 df = pd.concat(cond_res, ignore_index=True)
+#### df.to_excel('liniar_decoding_results.xlsx')
+
+
+
+
+presentation_period= 0.35 #stim presnetation time
+presentation_period_cue=  0.50 #presentation of attentional cue time
+pre_stim_period= 0.5 #time between cue and stim
+resp_time = 4  #time the response is active
+
+pal = sns.color_palette("tab10", n_colors=12, desat=1).as_hex()[0:3]
+
+
+fig = plt.figure(figsize=(10,8))
+for indx_c, condition in enumerate(['1_0.2', '1_7', '2_0.2', '2_7']): 
+    #features of the plot for the different conditions. Fixed values
+    if condition == '1_0.2':
+        delay1 = 0.2
+        delay2 = 11.8
+        cue=0
+        t_p = cue + presentation_period_cue + pre_stim_period 
+        d_p = t_p + presentation_period +delay1 
+        r_t = d_p + presentation_period + delay2
+        xlim = [0, 27]
+        
+    elif condition == '1_7':
+        delay1 = 7
+        delay2 = 5
+        cue=0
+        t_p = cue + presentation_period_cue + pre_stim_period 
+        d_p = t_p + presentation_period +delay1 
+        r_t = d_p + presentation_period + delay2
+        xlim = [0, 27]
+        
+    elif condition == '2_0.2':
+        delay1 = 0.2
+        delay2 = 12
+        cue=0
+        d_p = cue + presentation_period_cue + pre_stim_period 
+        t_p = d_p + presentation_period +delay1 
+        r_t = t_p + presentation_period + delay2   
+        xlim = [0, 27]
+        
+    elif condition == '2_7':
+        delay1 = 7
+        delay2 = 12
+        cue=0
+        d_p = cue + presentation_period_cue + pre_stim_period 
+        t_p = d_p + presentation_period +delay1 
+        r_t = t_p + presentation_period + delay2
+        xlim = [0, 35]
+        
+    
+    start_hrf = 4 #start of the Hemodynamic response (4seconds)
+    sec_hdrf = 3 #time it can last
+       
+    d_p1 = (start_hrf + d_p) ##strat of didtractor (time)
+    t_p1 = (start_hrf +t_p) ## strat of target (time)
+    r_t1=  (start_hrf + r_t) ## start of response (time)
+    #
+    d_p2 = d_p1 + sec_hdrf # end of distractor (time)
+    t_p2 = t_p1 + sec_hdrf # end of target (time)
+    r_t2=  r_t1 + sec_hdrf + resp_time #end of response (time)
+    
+    y_vl_min = 75 #df_all_by_subj.Decoding.min() #values min and max
+    y_vl_max = 150 #◙df_all_by_subj.Decoding.max()
+    
+    #fig = plt.figure()
+    ax = fig.add_subplot(2,2, indx_c+1) 
+    #ax = sns.lineplot(x='times', y='decoding',  color = 'black', data=n) #figure to get the intervals of shuffle
+    #ax.lines[0].set_linestyle("--")
+    sns.lineplot( ax=ax, x="time", y="error", hue='Brain_region', hue_order =  ['visual', 'ips', 'frontinf'],  ci=95, palette=pal, data=df.loc[ (df['condition']==condition)]) #, 'visual', 'ips',  'frontmid', 'frontsup', 'frontinf'
+    
+    #plt.plot([0, 35], [0,0], 'k--')   ## plot chance level (0)
+    plt.fill_between(  [ t_p1, t_p2 ], [y_vl_min, y_vl_min], [y_vl_max, y_vl_max], color='b', alpha=0.3) #, label='target'  ) #plot aprox time of target
+    plt.fill_between(  [ d_p1, d_p2 ], [y_vl_min, y_vl_min], [y_vl_max, y_vl_max], color='g', alpha=0.3) #, label='distractor'  ) #plot aprox time of distractor
+    plt.fill_between(  [ r_t1, r_t2 ], [y_vl_min, y_vl_min], [y_vl_max, y_vl_max], color='y', alpha=0.3) #, label='response'  )   #plot aprox time of response
+    TITLE_BR = condition 
+    plt.title(TITLE_BR) #condition title
+    plt.gca().spines['right'].set_visible(False) #no right axis
+    plt.gca().spines['top'].set_visible(False) #no  top axis
+    plt.gca().get_xaxis().tick_bottom()
+    plt.gca().get_yaxis().tick_left()
+    plt.xticks([5,15,25]) #just this tcks
+    plt.yticks([60,100,140])
+    plt.xlim(xlim)
+    plt.ylim(50,150)
+    if indx_c==3: #legend in just this condition (to avoid repetitions)       
+        plt.gca().legend(loc= 2, frameon=False)
+        plt.xticks([10, 20 ,30])
+        
+    else:
+        plt.gca().legend(loc= 1, frameon=False).remove()
+    
+
+
+##
+plt.suptitle( '', fontsize=18) ## main title
+plt.tight_layout(w_pad=5, h_pad=5, rect=[0, 0.03, 1, 0.95]) #correct the space between graphs
+plt.show(block=False) #show
+
+
+
+
+
+
+#### best and worst subject
+
 plt.figure()
-sns.lineplot(x='time', y='error', hue='condition', data=df.loc[df['Brain_region']=='visual'])
+sns.factorplot(x='Subject', y='error', kind='bar', data=df)
+plt.ylim(110, 130)
 plt.show(block=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
