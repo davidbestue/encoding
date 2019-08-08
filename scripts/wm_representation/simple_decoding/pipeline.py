3de119a6bbcdb8346433deada4b33f25eed82cdb
davidbestue <davidsanchezbestue@hotmail.com>
2019-08-08 15:38:02 +0200

    Update pipeline.py

--- scripts/wm_representation/simple_decoding/pipeline.py
+++ scripts/wm_representation/simple_decoding/pipeline.py
@@ -16,28 +16,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-
-
-def train_each_vxl( training_dataset, training_targets ):
-    #
-    ### X Training
-    ## X matrix (intercept and spikes)
-    X = np.column_stack([np.ones(np.shape(training_dataset)[0]),  training_dataset])
-    ## Y (sinus and cos of the target)
-    sinus =np.sin([np.radians(np.array(training_targets)[i]) for i in range(0, len(training_targets))])
-    cosinus = np.cos([np.radians(np.array(training_targets)[i]) for i in range(0, len(training_targets))])
-    Y = np.column_stack([cosinus, sinus])
-    Y = Y.astype(float) #to make it work in the cluster
-    X = X.astype(float)
-    model = sm.OLS(Y, X)
-    ##train the model
-    weights = model.fit()  ## training_weights.params
-    return weights
-
-
-
 nscans_wm = 18
-
 
 cond_res = []
 for Subject in ['n001', 'd001', 'r001', 's001', 'b001', 'l001']:
