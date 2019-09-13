a9d810804c6d4abf72be85c60138218fea248170
davidbestue <davidsanchezbestue@hotmail.com>
2019-09-13 12:39:46 +0200

    Update pipeline_example.py

--- scripts/wm_representation/functions/pipeline_example.py
+++ scripts/wm_representation/functions/pipeline_example.py
@@ -18,10 +18,14 @@ import random
 
 
 ### Data to use
-enc_fmri_paths, enc_beh_paths, wm_fmri_paths, wm_beh_paths, masks = data_to_use( 'n001', 'together', 'visual')
+enc_fmri_paths, enc_beh_paths, wm_fmri_paths, wm_beh_paths, masks = data_to_use( 'n001', 'together', 'ips')
 
 ##### Process training data
 training_dataset, training_targets = process_encoding_files(enc_fmri_paths, masks, enc_beh_paths, sys_use='unix', hd=4, TR=2.335)
+
+training_dataset.shape
+training_targets
+
 
 ##### Train your weigths
 WM, Inter = Weights_matrix_Lasso_i( training_dataset, training_targets )
