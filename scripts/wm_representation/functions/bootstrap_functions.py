a622965d847e649a85c939192757b31f277237e0
davidbestue <davidsanchezbestue@hotmail.com>
2019-09-17 16:12:47 +0200

    Update bootstrap_functions.py

--- scripts/wm_representation/functions/bootstrap_functions.py
+++ scripts/wm_representation/functions/bootstrap_functions.py
@@ -90,11 +90,22 @@ def bootstrap_reconstruction(testing_activity, targets, iterations, WM, WM_t, In
 
 
 
-def all_process_condition_shuff_boot( Subject, Brain_Region, WM, WM_t, Inter, Condition, iterations, distance, method='together', heatmap=False):
+def all_process_condition_shuff_boot( Subject, Brain_Region, WM, WM_t, Inter, Condition, iterations, distance, decode_item, method='together', heatmap=False):
     enc_fmri_paths, enc_beh_paths, wm_fmri_paths, wm_beh_paths, masks = data_to_use( Subject, method, Brain_Region)
     ##### Process testing data
     testing_activity, testing_behaviour = preprocess_wm_files(wm_fmri_paths, masks, wm_beh_paths, condition=Condition, distance=distance, sys_use='unix', nscans_wm=nscans_wm, TR=2.335)
-    testing_angles = np.array(testing_behaviour['Dist'])    
+
+    if decode_item == 'Target':
+    	dec_I = 'T'
+    elif decode_item == 'Response':
+    	dec_I = 'A_R'
+    elif decode_item == 'Distractor':
+    	dec_I = 'Dist'
+    else:
+    	'Error specifying the decode item'
+
+    #
+    testing_angles = np.array(testing_behaviour[dec_I])    # A_R # T # Dist
     ### Respresentation
     start_repres = time.time()    
     # TR separartion
