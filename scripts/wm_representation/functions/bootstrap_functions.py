9e8732c087d3ffd2e298b3597be6bdfd55e15dd1
davidbestue <davidsanchezbestue@hotmail.com>
2019-10-09 16:45:50 +0200

    update

--- scripts/wm_representation/functions/bootstrap_functions.py
+++ scripts/wm_representation/functions/bootstrap_functions.py
@@ -142,6 +142,67 @@ def all_process_condition_shuff_boot( Subject, Brain_Region, WM, WM_t, Inter, Co
 
 
 
+
+
+def all_process_condition_shuff( Subject, Brain_Region, WM, WM_t, Inter, Condition, iterations, distance, decode_item, method='together', heatmap=False):
+    enc_fmri_paths, enc_beh_paths, wm_fmri_paths, wm_beh_paths, masks = data_to_use( Subject, method, Brain_Region)
+    ##### Process testing data
+    testing_activity, testing_behaviour = preprocess_wm_files(wm_fmri_paths, masks, wm_beh_paths, condition=Condition, distance=distance, sys_use='unix', nscans_wm=nscans_wm, TR=2.335)
+
+    if decode_item == 'Target':
+        dec_I = 'T'
+    elif decode_item == 'Response':
+        dec_I = 'A_R'
+    elif decode_item == 'Distractor':
+        dec_I = 'Dist'
+    else:
+        'Error specifying the decode item'
+
+    #
+    testing_angles = np.array(testing_behaviour[dec_I])    # A_R # T # Dist
+    ### Respresentation
+    start_repres = time.time()    
+    # TR separartion
+    signal_paralel =[ testing_activity[:, i, :] for i in range(nscans_wm)]
+    Reconstructions = Parallel(n_jobs = numcores)(delayed(Representation)(signal, testing_angles, WM, WM_t, ref_angle=180, plot=False, intercept=Inter)  for signal in signal_paralel)    #### reconstruction standard (paralel)
+    Reconstruction = pd.concat(Reconstructions, axis=1) #mean of the reconstructions (all trials)
+    Reconstruction.columns =  [str(i * TR) for i in range(nscans_wm)]    ##column names
+    #Plot heatmap
+    if heatmap==True:
+        plt.figure()
+        plt.title(Condition)
+        ax = sns.heatmap(Reconstruction, yticklabels=list(Reconstruction.index), cmap="coolwarm") # cmap= viridis "jet",  "coolwarm" RdBu_r, gnuplot, YlOrRd, CMRmap  , center = midpoint
+        ax.plot([0.25, np.shape(Reconstruction)[1]-0.25], [posch1_to_posch2(18),posch1_to_posch2(18)], 'k--')
+        plt.yticks([posch1_to_posch2(4), posch1_to_posch2(13), posch1_to_posch2(22), posch1_to_posch2(31)] ,['45','135','225', '315'])
+        plt.ylabel('Angle')
+        plt.xlabel('time (s)')
+        plt.tight_layout()
+        plt.show(block=False)
+    
+    ######
+    ######
+    ######
+    end_repres = time.time()
+    process_recons = end_repres - start_repres
+    print( 'Time process reconstruction: ' +str(process_recons)) #print time of the process
+    
+    #df_boots = bootstrap_reconstruction(testing_activity, testing_angles, iterations, WM, WM_t, Inter, Brain_Region, Condition, Subject, ref_angle=180)    
+    ####### Shuff
+    #### Compute the shuffleing
+    shuffled_rec = shuffled_reconstruction(signal_paralel, testing_angles, iterations, WM, WM_t, Inter=Inter, region=Brain_Region, condition=Condition, subject=Subject, ref_angle=180)
+    
+    return Reconstruction, shuffled_rec
+
+
+
+
+
+
+
+
+
+
+
 #############################
 #############################
 #############################
