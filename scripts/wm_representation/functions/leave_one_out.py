b9a24b359de54c33559e9dc0875878b13668372d
David Bestue <davidsanchezbestue@hotmail.com>
2020-08-06 12:07:20 +0200

    update

--- scripts/wm_representation/functions/leave_one_out.py
+++ scripts/wm_representation/functions/leave_one_out.py
@@ -14,6 +14,44 @@ import random
 from sklearn.model_selection import LeaveOneOut
 
 
+def Pop_vect_angle(testing_data, testing_angles, ref_angle=180):
+	## A esta función entrarán
+    ## Make the data parallelizable
+    n_trials_test = len(testing_data) #number trials
+    data_prall = []
+    for i in range(n_trials_test):
+        data_prall.append(testing_data[i, :])
+        #data_prall.append(    np.array( stats.zscore(    testing_data[i, :] ))   ) ###what enters the formula is zscored!
+        
+        
+    
+    
+    ###
+    numcores = multiprocessing.cpu_count()
+    Channel_all_trials_rolled = Parallel(n_jobs = numcores)(delayed(trial_rep)(Signal, angle_trial, Weights, Weights_t, ref=ref_angle, intercept_ = intercept)  for Signal, angle_trial in zip( data_prall, testing_angles))    ####
+    #Channel_all_trials_rolled = Parallel(n_jobs = numcores)(delayed(trial_rep_decode_trial_by_trial)(Signal, angle_trial, Weights, Weights_t, ref=ref_angle, intercept_ = intercept)  for Signal, angle_trial in zip( data_prall, testing_angles))    ####
+    Channel_all_trials_rolled = np.array(Channel_all_trials_rolled)
+    
+    df = pd.DataFrame()
+    n = list(Channel_all_trials_rolled.mean(axis=0)) #mean of all the trials rolled
+    df['TR'] = n #Name of the column
+    if plot==True:
+        #Plot heatmap
+        plt.figure()
+        plt.title('Heatmap decoding')
+        ######midpoint = df.values.mean() # (df.values.max() - df.values.min()) / 2
+        ax = sns.heatmap(df, yticklabels=list(df.index), cmap="coolwarm") # cmap= viridis "jet",  "coolwarm" RdBu_r, gnuplot, YlOrRd, CMRmap  , center = midpoint
+        ax.plot([0.25, np.shape(df)[1]-0.25], [posch1_to_posch2(18),posch1_to_posch2(18)], 'k--')
+        plt.yticks([posch1_to_posch2(4), posch1_to_posch2(13), posch1_to_posch2(22), posch1_to_posch2(31)] ,['45','135','225', '315'])
+        plt.ylabel('Angle')
+        plt.xlabel('time (s)')
+        plt.show(block=False)
+    
+    return df
+
+
+
+
 def leave_one_out_shuff( Subject, Brain_Region, Condition, iterations, distance, decode_item, method='together', heatmap=False):
     enc_fmri_paths, enc_beh_paths, wm_fmri_paths, wm_beh_paths, masks = data_to_use( Subject, method, Brain_Region)
     ##### Process testing data
