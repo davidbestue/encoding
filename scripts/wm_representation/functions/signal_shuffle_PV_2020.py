c4a20faeee7cb547c91673095f1826651cea4a75
David Bestue <davidsanchezbestue@hotmail.com>
2020-08-07 19:09:09 +0200

    update

--- scripts/wm_representation/functions/signal_shuffle_PV_2020.py
+++ scripts/wm_representation/functions/signal_shuffle_PV_2020.py
@@ -18,29 +18,27 @@ import multiprocessing
 import time
 import random
 #
-
 numcores = multiprocessing.cpu_count() #- 10
 
 ##paths to save the 3 files 
 decoding_thing = 'Target' #'Distractor' #'Target'
-Distance_to_use = 'close'
-#path_save_reconstructions = '/home/david/Desktop/all_target_mix.xlsx' 
-#path_save_signal ='/home/david/Desktop/target_close/signal_all_target_mix.xlsx'
-#path_save_shuff = '/home/david/Desktop/target_close/shuff_all_target_mix.xlsx'
+Distance_to_use = 'mix'
+
+path_save_signal ='/home/david/Desktop/leave1out/signal_all_target_mix_l1o.xlsx'
+path_save_shuffle = '/home/david/Desktop/leave1out/shuff_all_target_mix_l1o.xlsx'
+
 Reconstructions=[]
 Reconstructions_shuff=[]
 
-Conditions=['1_0.2']#, '1_7', '2_0.2', '2_7'] #
-Subjects=['d001', 'n001'] #, 'n001', 'b001', 'r001', 's001', 'l001'] #, 'r001', 'd001', 'b001', 's001', 'l001'
-brain_regions = ['visual'] #, 'ips', 'pfc']# 'frontinf'] #, 'ips', 'frontsup', 'frontmid', 'frontinf'
-
-
+Conditions=['1_0.2', '1_7', '2_0.2', '2_7'] #
+Subjects=['d001', 'n001', 'b001', 'r001', 's001', 'l001'] #, 'r001', 'd001', 'b001', 's001', 'l001'
+brain_regions = ['visual', 'ips', 'pfc']# 'frontinf'] #, 'ips', 'frontsup', 'frontmid', 'frontinf'
 
 for Subject in Subjects:
     for Brain_region in brain_regions:
         for idx_c, Condition in enumerate(Conditions):
             print(Condition)
-            Reconstruction, shuff = leave_one_out_shuff( Subject=Subject, Brain_Region=Brain_region, Condition=Condition, iterations=5, 
+            Reconstruction, shuff = leave_one_out_shuff( Subject=Subject, Brain_Region=Brain_region, Condition=Condition, iterations=100, 
                 distance=Distance_to_use, decode_item=decoding_thing, method='together', heatmap=False)
             ##
             Reconstructions.append(Reconstruction)
@@ -49,10 +47,11 @@ for Subject in Subjects:
 
 
 ###
-
 ### Save signal from the reconstructions and shuffles
 Decoding_df = pd.concat(Reconstructions, axis=0) 
 Decoding_df['label']='signal'
+Decoding_df.to_excel( path_save_signal )
+
 Shuffle_df = pd.concat(Reconstructions_shuff, axis=0) 
 Shuffle_df['label']='shuffle'
-
+Shuffle_df.to_excel( path_save_shuffle )
