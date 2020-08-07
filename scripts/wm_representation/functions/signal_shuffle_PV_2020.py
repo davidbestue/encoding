995cfff5f1c82a412928c46027dc223261426a6d
David Bestue <davidsanchezbestue@hotmail.com>
2020-08-07 13:44:13 +0200

    update

--- scripts/wm_representation/functions/signal_shuffle_PV_2020.py
+++ scripts/wm_representation/functions/signal_shuffle_PV_2020.py
@@ -65,60 +65,42 @@ for Subject in Subjects:
 #    Brain_Region=Brain_region, Condition=Condition, iterations=2, distance=Distance_to_use, decode_item=decoding_thing, method='together', heatmap=False)
 
 
-# Subject='d001'
-# method='together' 
-# Brain_Region='visual'
-# enc_fmri_paths, enc_beh_paths, wm_fmri_paths, wm_beh_paths, masks = data_to_use( Subject, 'together', Brain_region)
-# Condition='1_0.2'
-# Subject='d001'
-# distance='close'
-# testing_activity, testing_behaviour = preprocess_wm_files(wm_fmri_paths, masks, wm_beh_paths, condition=Condition, distance=distance, sys_use='unix', nscans_wm=nscans_wm, TR=2.335)
-# decode_item = 'Target'
-# dec_I = 'T'
-# testing_angles_beh = np.array(testing_behaviour[dec_I])    # A_R # T # Dist
-# angles_paralel= [testing_angles_beh for i in range(nscans_wm)]
-# signal_paralel =[ testing_activity[:, i, :] for i in range(nscans_wm)]
+Subject='d001'
+method='together' 
+Brain_Region='visual'
+enc_fmri_paths, enc_beh_paths, wm_fmri_paths, wm_beh_paths, masks = data_to_use( Subject, 'together', Brain_region)
+Condition='1_0.2'
+Subject='d001'
+distance='close'
+testing_activity, testing_behaviour = preprocess_wm_files(wm_fmri_paths, masks, wm_beh_paths, condition=Condition, distance=distance, sys_use='unix', nscans_wm=nscans_wm, TR=2.335)
+decode_item = 'Target'
+dec_I = 'T'
+testing_angles_beh = np.array(testing_behaviour[dec_I])    # A_R # T # Dist
+angles_paralel= [testing_angles_beh for i in range(nscans_wm)]
+signal_paralel =[ testing_activity[:, i, :] for i in range(nscans_wm)]
 
 
 
-# testing_data=signal_paralel[0]
-# testing_angles=angles_paralel[0]
+testing_data=signal_paralel[0]
+testing_angles=angles_paralel[0]
 
-# loo = LeaveOneOut()
-# errors_=[]
-# for train_index, test_index in loo.split(testing_data):
-#     X_train, X_test = testing_data[train_index], testing_data[test_index]
-#     y_train, y_test = testing_angles[train_index], testing_angles[test_index]
-#     ##
-#     ## correr el modelo en cada uno de los sets y guardar el error en cada uno de los trials
-#     ## la std no la hare con estos errores, sinó con el shuffle. No necesito guardar el error en cada repetición.
-# ###
-
-# X = np.column_stack([np.ones(np.shape(X_train)[0]), X_train])
-# ## Y (sinus and cos)
-# sinus =np.sin([np.radians(np.array(y_train)[i]) for i in range(0, len(y_train))])
-# cosinus = np.cos([np.radians(np.array(y_train)[i]) for i in range(0, len(y_train))])
-# Y = np.column_stack([cosinus, sinus])
-# ### one OLS for sin and cos: output: beta of intercetp and bea of spikes (two B intercepts and 2 B for spikes )
-# Y = Y.astype(float) #to make it work in the cluster
-# X = X.astype(float)
-# model = sm.OLS(Y, X)
-# ##train the model
-# fit=model.fit()
-# ######### Testing the remaining one ###########
-# X_ = np.column_stack([np.ones(np.shape(X_test)[0]), X_test])
-# p = fit.predict(X_)
-# x = p[0][0]
-# y = p[0][1]
-# #####
-# ##### Error --> take the resulting vector in sin/cos space
-# ### from sin and cos get the angle (-pi, pi)
-# pred_angle = np.degrees(np.arctan2(y, x)) 
-# if pred_angle<0:
-#         pred_angle=360+pred_angle
-# ##
-# #print(pred_angle, y_test)
-# error_ = err_deg(pred_angle, y_test)
-
-# model_trained_err = model_PV(X_train, X_test, y_train, y_test)
-# errors_.append(model_trained_err)
+loo = LeaveOneOut()
+errors_=[]
+for train_index, test_index in loo.split(testing_data):
+    X_train, X_test = testing_data[train_index], testing_data[test_index]
+    y_train, y_test = testing_angles[train_index], testing_angles[test_index]
+    ##
+    ## correr el modelo en cada uno de los sets y guardar el error en cada uno de los trials
+    ## la std no la hare con estos errores, sinó con el shuffle. No necesito guardar el error en cada repetición.
+    loo = LeaveOneOut()
+    errors_=[]
+    for train_index, test_index in loo.split(testing_data):
+        X_train, X_test = testing_data[train_index], testing_data[test_index]
+        y_train, y_test = testing_angles[train_index], testing_angles[test_index]
+        ##
+        ## correr el modelo en cada uno de los sets y guardar el error en cada uno de los trials
+        ## la std no la hare con estos errores, sinó con el shuffle. No necesito guardar el error en cada repetición.
+        model_trained_err = model_PV(X_train, X_test, y_train, y_test)
+        errors_.append(model_trained_err)
+    ##
+    error = np.mean(errors_)
