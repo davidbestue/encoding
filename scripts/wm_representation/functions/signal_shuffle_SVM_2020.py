7decfc16675afae9d41f3fedc0be9e5e2173bc1e
David Bestue <davidsanchezbestue@hotmail.com>
2020-09-09 10:57:44 +0200

    update

--- scripts/wm_representation/functions/signal_shuffle_SVM_2020.py
+++ scripts/wm_representation/functions/signal_shuffle_SVM_2020.py
@@ -5,7 +5,6 @@ Created on Mon Jul  1 18:24:32 2019
 @author: David Bestue
 """
 from model_functions import *
-from fake_data_generator import *
 from Weights_matrixs import *
 from Representation import *
 from process_encoding import *
@@ -17,5 +16,9 @@ from joblib import Parallel, delayed
 import multiprocessing
 import time
 import random
+from sklearn import svm
+
 #
 numcores = multiprocessing.cpu_count() - 10
+
+
