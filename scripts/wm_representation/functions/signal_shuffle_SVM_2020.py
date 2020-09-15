8b468939560443da01f2832ec879f4eb4731388d
David Bestue <davidsanchezbestue@hotmail.com>
2020-09-15 17:44:26 +0200

    update

--- scripts/wm_representation/functions/signal_shuffle_SVM_2020.py
+++ scripts/wm_representation/functions/signal_shuffle_SVM_2020.py
@@ -5,85 +5,54 @@ Created on Mon Jul  1 18:24:32 2019
 @author: David Bestue
 """
 from model_functions import *
+from fake_data_generator import *
 from Weights_matrixs import *
 from Representation import *
 from process_encoding import *
 from process_wm import *
 from data_to_use import *
 from bootstrap_functions import *
-from leave_one_out import *
+#from leave_one_out import *
+from support_vector_machine import *
 from joblib import Parallel, delayed
 import multiprocessing
 import time
 import random
-from sklearn import svm
-
 #
 numcores = multiprocessing.cpu_count() - 10
 
+##paths to save the 3 files 
+decoding_thing = 'Distractor' #'Distractor' #'Target'
+Distance_to_use = 'mix'
 
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn import svm
+path_save_signal ='/home/david/Desktop/leave1out/signal_all_distractor_mix_l1out.xlsx'
+path_save_shuffle = '/home/david/Desktop/leave1out/shuff_all_distractor_mix_l1out.xlsx'
 
-xx, yy = np.meshgrid(np.linspace(-3, 3, 500),
-                     np.linspace(-3, 3, 500))
-np.random.seed(0)
-X = np.random.randn(300, 2)
-Y = np.logical_xor(X[:, 0] > 0, X[:, 1] > 0) * 1
+Reconstructions=[]
+Reconstructions_shuff=[]
 
-#a=[1,2,3,4]
-#Y=np.array([random.choice(a) for i in range(300)] )# elegir de a n veces
+Conditions=['1_0.2', '1_7', '2_0.2', '2_7'] #
+Subjects=['d001', 'n001', 'b001', 'r001', 's001', 'l001'] #, 'r001', 'd001', 'b001', 's001', 'l001'
+brain_regions = ['visual', 'ips', 'pfc']# 'frontinf'] #, 'ips', 'frontsup', 'frontmid', 'frontinf'
 
-
-#################################################################
-################################################################# Desde train and test
-#################################################################
-
-X_train = np.random.randn(300, 1)
-y_train = Y = np.random.randint(0,3, 300) 
-
-X_test = np.random.randn(300, 1)
-y_test = Y = np.random.randint(0,3, 300) 
-
-# fit the model
-clf = svm.NuSVC(gamma='auto')
-clf.fit(X_train, Y_train)
-
-prediction = clf.predict(X_test)
-decoding = np.mean(Y_test==prediction)  ### npercentage of correct classifications
-
-#
-shuffl_dec = []
-
-for i in range(4):
-    Y_shuffl = Y = np.random.randint(0,3, 300) 
-    decoding = np.mean(Y_shuffl==prediction) 
-    shuffl_dec.append(decoding)
+for Subject in Subjects:
+    for Brain_region in brain_regions:
+        for idx_c, Condition in enumerate(Conditions):
+            print(Subject + ', ' + Brain_region +', ' + Condition)
+            Reconstruction, shuff = leave_one_out_shuff( Subject=Subject, Brain_Region=Brain_region, Condition=Condition, iterations=100, 
+                distance=Distance_to_use, decode_item=decoding_thing, method='together', heatmap=False) #100
+            ##
+            Reconstructions.append(Reconstruction)
+            #Reconstructions_boots.append(boots)
+            Reconstructions_shuff.append(shuff)
 
 
+###
+### Save signal from the reconstructions and shuffles
+Decoding_df = pd.concat(Reconstructions, axis=0) 
+Decoding_df['label']='signal'
+Decoding_df.to_excel( path_save_signal )
 
-######################################################################
-###################################################################### Desde leave one out
-###################################################################### 
-
-
-### error in the fit???'' hoooow????
-
-# plot the decision function for each datapoint on the grid
-Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
-Z = Z.reshape(xx.shape)
-
-plt.imshow(Z, interpolation='nearest',
-           extent=(xx.min(), xx.max(), yy.min(), yy.max()), aspect='auto',
-           origin='lower', cmap=plt.cm.PuOr_r)
-contours = plt.contour(xx, yy, Z, levels=[0], linewidths=2,
-                       linestyles='dashed')
-plt.scatter(X[:, 0], X[:, 1], s=30, c=Y, cmap=plt.cm.Paired,
-            edgecolors='k')
-plt.xticks(())
-plt.yticks(())
-plt.axis([-3, 3, -3, 3])
-plt.show(block=False)
-
-
+Shuffle_df = pd.concat(Reconstructions_shuff, axis=0) 
+Shuffle_df['label']='shuffle'
+Shuffle_df.to_excel( path_save_shuffle )
