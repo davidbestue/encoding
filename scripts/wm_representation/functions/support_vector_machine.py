216988e1b00ad355ee622f8f157b05207883a2db
David Bestue <davidsanchezbestue@hotmail.com>
2020-09-23 15:00:05 +0200

    update

--- scripts/wm_representation/functions/support_vector_machine.py
+++ scripts/wm_representation/functions/support_vector_machine.py
@@ -35,7 +35,7 @@ def get_quadrant(angle):
 def model_SVM(X_train, X_test, y_train, y_test):
     ##
     ######## Trainning #########
-    ker = 'lineal'
+    ker = 'linear'
     clf = svm.NuSVC(gamma='auto', kernel=ker,  nu=0.1)
     clf.fit(X_train, y_train)
     ######## Testing ##########
--- scripts/wm_representation/functions/support_vector_machine_octaves.py
+++ scripts/wm_representation/functions/support_vector_machine_octaves.py
@@ -46,7 +46,7 @@ def get_octave(angle):
 def model_SVM(X_train, X_test, y_train, y_test):
     ##
     ######## Trainning #########
-    ker = 'lineal'
+    ker = 'linear'
     clf = svm.NuSVC(gamma='auto', kernel=ker,  nu=0.1)
     clf.fit(X_train, y_train)
     ######## Testing ##########
