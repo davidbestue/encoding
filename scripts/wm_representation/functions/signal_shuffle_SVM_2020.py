096aa33198a9c2fc203d9fef0a028c81d3b6b69f
David Bestue <davidsanchezbestue@hotmail.com>
2020-09-24 19:27:05 +0200

    update

--- scripts/wm_representation/functions/signal_shuffle_SVM_2020.py
+++ scripts/wm_representation/functions/signal_shuffle_SVM_2020.py
@@ -34,9 +34,9 @@ path_save_shuffle = '/home/david/Desktop/Reconstructions/SVM/shuff_all_target_mi
 Reconstructions=[]
 Reconstructions_shuff=[]
 
-Conditions=['1_0.2', '1_7', '2_0.2', '2_7'] #
-Subjects=['b001', 'n001', 'd001', 'r001', 's001', 'l001'] #, 'r001', 'd001', 'b001', 's001', 'l001'
-brain_regions = ['visual', 'ips', 'pfc']# 'frontinf'] #, 'ips', 'frontsup', 'frontmid', 'frontinf'
+Conditions=['1_0.2'] #, '1_7', '2_0.2', '2_7'] #
+Subjects=['b001'] #, 'n001', 'd001', 'r001', 's001', 'l001'] #, 'r001', 'd001', 'b001', 's001', 'l001'
+brain_regions = ['visual']#, 'ips', 'pfc']# 'frontinf'] #, 'ips', 'frontsup', 'frontmid', 'frontinf'
 
 for Subject in Subjects:
     for Brain_region in brain_regions:
