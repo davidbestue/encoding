0f50e3c6de433f24b41c06b49297a457c767d42a
David Bestue <davidsanchezbestue@hotmail.com>
2020-10-21 20:17:07 +0200

    update

--- scripts/wm_representation/functions/cross_temporal_decoding_condition6.py
+++ scripts/wm_representation/functions/cross_temporal_decoding_condition6.py
@@ -25,7 +25,7 @@ numcores = multiprocessing.cpu_count() - 2
 
 ##paths to save the 3 files 
 decoding_thing = 'Distractor' #'Distractor' #'Target'
-Distance_to_use = 'far'
+Distance_to_use = 'close'
 
 path_save_signal ='/home/david/Desktop/Reconstructions/SVM/cross_dist_close_delay_2_7.xlsx' #cross_b001_target_mix_octave_1_7_far.xlsx'
 path_save_shuffle = '/home/david/Desktop/Reconstructions/SVM/shuff_cross_dist_close_delay_2_7.xlsx'
