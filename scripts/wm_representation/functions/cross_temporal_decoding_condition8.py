11eca65400398bc59e1240805bfddae697d12427
David Bestue <davidsanchezbestue@hotmail.com>
2020-10-22 08:57:20 +0200

    update

--- scripts/wm_representation/functions/cross_temporal_decoding_condition8.py
+++ scripts/wm_representation/functions/cross_temporal_decoding_condition8.py
@@ -21,7 +21,7 @@ import multiprocessing
 import time
 import random
 #
-numcores = multiprocessing.cpu_count() - 2
+numcores = multiprocessing.cpu_count() - 8
 
 ##paths to save the 3 files 
 decoding_thing = 'Target' #'Distractor' #'Target'
