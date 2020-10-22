46fd6075cfeaef757ffac3c37e9c63ea32abb0fb
David Bestue <davidsanchezbestue@hotmail.com>
2020-10-22 11:06:29 +0200

    update

--- scripts/wm_representation/functions/cross_temporal_decoding_condition.py
+++ scripts/wm_representation/functions/cross_temporal_decoding_condition.py
@@ -27,15 +27,15 @@ numcores = multiprocessing.cpu_count() - 10
 decoding_thing = 'Distractor' #'Distractor' #'Target'
 Distance_to_use = 'far'
 
-path_save_signal ='/home/david/Desktop/Reconstructions/SVM/cross_dist_far_delay_2_7.xlsx' #cross_b001_target_mix_octave_1_7_far.xlsx'
-path_save_shuffle = '/home/david/Desktop/Reconstructions/SVM/shuff_cross_dist_far_delay_2_7.xlsx'
+path_save_signal ='/home/david/Desktop/Reconstructions/SVM/b001_ex_dist_far_delay_2_7.xlsx' #cross_b001_target_mix_octave_1_7_far.xlsx'
+path_save_shuffle = '/home/david/Desktop/Reconstructions/SVM/b001_ex_dist_far_delay_2_7.xlsx'
 
 matrixs={}
 matrixs_shuff=[]
 
-Conditions=['1_0.2', '1_7', '2_0.2', '2_7'] #
-Subjects=['b001', 'n001', 'd001', 'r001', 's001', 'l001'] #, 'r001', 'd001', 'b001', 's001', 'l001'
-brain_regions = ['visual', 'ips', 'pfc']# 'frontinf'] #, 'ips', 'frontsup', 'frontmid', 'frontinf'
+Conditions=['1_0.2', '2_7'] #
+Subjects=['b001'] #, 'r001', 'd001', 'b001', 's001', 'l001'
+brain_regions = ['visual']# 'frontinf'] #, 'ips', 'frontsup', 'frontmid', 'frontinf'
 
 sh_reps = 2
 
