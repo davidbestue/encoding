6cd8456c02f2df543530b066ab906e9a8788a81b
David Bestue <davidsanchezbestue@hotmail.com>
2020-10-23 11:22:43 +0200

    update

--- scripts/wm_representation/functions/CTD_conditions/SVM_decoding_condition6.py
+++ scripts/wm_representation/functions/CTD_conditions/SVM_decoding_condition6.py
@@ -45,7 +45,6 @@ elif decoding_thing=='Target':
 #
 if training_time=='stim_p':
     tr_st = 2
-    tr_end= 3
     
 elif training_time=='delay':
     tr_st = 3
@@ -72,7 +71,7 @@ for Subject in Subjects:
             #
             #training activity
             if training_time=='stim_p':
-                delay_TR_cond = training_activity[:, tr_st:tr_end, :]
+                delay_TR_cond = training_activity[:, tr_st, :]
             if training_time=='delay':
                 delay_TR_cond = np.mean(training_activity[:, tr_st:tr_end, :], axis=1) ## training_activity[:, 8, :]
             
