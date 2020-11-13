5ee19e2fcd9f895a6ba69b60e8f11e71efbb9f06
David Bestue <davidsanchezbestue@hotmail.com>
2020-11-13 14:20:48 +0100

    update

--- scripts/wm_representation/functions/CTD_conditions/SVM_decoding_condition8.py
+++ scripts/wm_representation/functions/CTD_conditions/SVM_decoding_condition8.py
@@ -28,8 +28,8 @@ import random
 numcores = multiprocessing.cpu_count() - 10
 
 ##paths to save the 3 files 
-path_save_signal ='/home/david/Desktop/Reconstructions/SVM/cross_target_far_stim.xlsx' #cross_b001_target_mix_octave_1_7_far.xlsx'
-path_save_shuffle = '/home/david/Desktop/Reconstructions/SVM/shuff_cross_target_far_stim.xlsx'
+path_save_signal ='/home/david/Desktop/Reconstructions/SVM/cross_target_far_stim3.xlsx' #cross_b001_target_mix_octave_1_7_far.xlsx'
+path_save_shuffle = '/home/david/Desktop/Reconstructions/SVM/shuff_cross_target_far_stim3.xlsx'
 
 decoding_thing = 'Target' #'Distractor' #'Target'
 Distance_to_use = 'far'
@@ -43,11 +43,11 @@ elif decoding_thing=='Target':
     cond_t = '1_7'
 #
 if training_time=='stim_p':
-    tr_st = 2    
+    tr_st = 3    
     
 elif training_time=='delay':
-    tr_st = 3
-    tr_end= 5
+    tr_st = 4
+    tr_end= 6
 
 #
 matrixs={}
