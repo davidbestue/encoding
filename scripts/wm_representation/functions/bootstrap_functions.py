97dccabb342f1f977a164fb096e7e0c27e63d6aa
David Bestue <davidsanchezbestue@hotmail.com>
2020-11-25 13:00:48 +0100

    update

--- scripts/wm_representation/functions/bootstrap_functions.py
+++ scripts/wm_representation/functions/bootstrap_functions.py
@@ -267,7 +267,7 @@ def IEM_cross_condition_kfold_shuff(testing_activity, testing_behaviour, decode_
     ####
     ####
     #### IEM usando data de WM test
-    #### IEM de aquellos TRs donde se use también training data (condiciones 1_7 y 2_7)
+    #### IEM de aquellos TRs donde se use tambien training data (condiciones 1_7 y 2_7)
     #### En vez de hacer leave one out, que tarda mucho, o usar el mismo data (overfitting), hago k_fold, con 10 splits. 
     #### Añado el shuffle al principio según el numero de iterations
     ####
