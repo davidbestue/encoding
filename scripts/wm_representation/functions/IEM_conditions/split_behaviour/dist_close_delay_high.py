07bb7e6d1ece57dcdad236dee8f78a456811a468
David Bestue <davidsanchezbestue@hotmail.com>
2020-12-15 17:56:58 +0100

    update

--- scripts/wm_representation/functions/IEM_conditions/split_behaviour/dist_close_delay_high.py
+++ scripts/wm_representation/functions/IEM_conditions/split_behaviour/dist_close_delay_high.py
@@ -80,7 +80,6 @@ elif training_time=='respo':
 Reconstructions={}
 Reconstructions_shuff=[]
 
-
 ## elements for the loop
 Conditions=['1_0.2', '2_0.2'] # '1_0.2', '1_7', '2_0.2', '2_7'
 Subjects=['d001', 'n001', 'b001', 'r001', 's001', 'l001'] #'d001', 'n001', 'b001', 'r001', 's001', 'l001'
