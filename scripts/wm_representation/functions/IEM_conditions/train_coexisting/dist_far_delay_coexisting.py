fec3d1ececb039cd49cc94dbe7311cb35d323892
David Bestue <davidsanchezbestue@hotmail.com>
2021-01-26 17:32:36 +0100

    update

--- scripts/wm_representation/functions/IEM_conditions/train_coexisting/dist_far_delay_coexisting.py
+++ scripts/wm_representation/functions/IEM_conditions/train_coexisting/dist_far_delay_coexisting.py
@@ -85,7 +85,7 @@ Subjects=['d001', 'n001', 'b001', 'r001', 's001', 'l001'] #'d001', 'n001', 'b001
 brain_regions = ['visual', 'ips', 'pfc'] # 'visual', 'ips', 'pfc'
 ref_angle=180
 
-num_shuffles = 2 #00
+num_shuffles = 100 #00
 
 for Subject in Subjects:
     print(Subject)
