8288f362fc2b6d85e85031793f29b8941205a1ee
David Bestue <davidsanchezbestue@hotmail.com>
2021-02-12 13:30:51 +0100

    update

--- scripts/wm_representation/functions/isolation_reconstruction.py
+++ scripts/wm_representation/functions/isolation_reconstruction.py
@@ -19,9 +19,7 @@ numcores = multiprocessing.cpu_count()
 
 df = pd.read_excel('C:\\Users\\David\Desktop\\KI_Desktop\\data_reconstructions\\IEM\\example2_beh.xlsx') 
 
-
 targets_distractors = df[['T', 'NT1', 'NT2', 'Dist', 'Dist_NT1', 'Dist_NT2']]
-
 
 targets_distractors['q_t1'] = [get_quad(targets_distractors.iloc[i]['T']) for i in range(len(targets_distractors))]
 targets_distractors['q_nt1'] = [get_quad(targets_distractors.iloc[i]['NT1']) for i in range(len(targets_distractors))]
@@ -34,6 +32,7 @@ targets_distractors['q_dist2'] = [get_quad(targets_distractors.iloc[i]['Dist_NT2
 
 
 targets_alone_quadrant=[]
+distractor_alone_quadrant=[]
 
 for i in range(len(targets_distractors)):
     targets_quadrants = [targets_distractors['q_t1'].iloc[i], targets_distractors['q_nt1'].iloc[i], targets_distractors['q_nt2'].iloc[i]]                                                                                 
@@ -41,8 +40,30 @@ for i in range(len(targets_distractors)):
     ##################
     ################## get target alone
     ##################
-    if tragets_quadrants[0] not in distractors_quadrants:
-        targets_alone_quadrant.append
+    if targets_quadrants[0] not in distractors_quadrants:
+        targets_alone_quadrant.append(targets_distractors.iloc[i]['T'])
+    elif targets_quadrants[1] not in distractors_quadrants:
+        targets_alone_quadrant.append(targets_distractors.iloc[i]['NT1'])
+    elif targets_quadrants[2] not in distractors_quadrants:
+        targets_alone_quadrant.append(targets_distractors.iloc[i]['NT2'])
+    else:
+        print('Error distribution stimuli')
+    ##################
+    ################## get distractor alone
+    ##################
+    if distractors_quadrants[0] not in targets_quadrants:
+        distractor_alone_quadrant.append(targets_distractors.iloc[i]['Dist'])
+    elif distractors_quadrants[1] not in targets_quadrants:
+        distractor_alone_quadrant.append(targets_distractors.iloc[i]['Dist_NT1'])
+    elif distractors_quadrants[2] not in targets_quadrants:
+        distractor_alone_quadrant.append(targets_distractors.iloc[i]['Dist_NT2'])
+    else:
+        print('Error distribution stimuli')
+
+
+df['T_alone'] = targets_alone_quadrant
+df['dist_alone'] = distractor_alone_quadrant
+
 
 
 
@@ -67,7 +88,51 @@ def get_quad(degree):
 
 
 
-def isolated_one
+def isolated_one(behaviour):
+    targets_distractors = behaviour[['T', 'NT1', 'NT2', 'Dist', 'Dist_NT1', 'Dist_NT2']]
+    ###
+    targets_distractors['q_t1'] = [get_quad(targets_distractors.iloc[i]['T']) for i in range(len(targets_distractors))]
+    targets_distractors['q_nt1'] = [get_quad(targets_distractors.iloc[i]['NT1']) for i in range(len(targets_distractors))]
+    targets_distractors['q_nt2'] = [get_quad(targets_distractors.iloc[i]['NT2']) for i in range(len(targets_distractors))]
+    targets_distractors['q_dist'] = [get_quad(targets_distractors.iloc[i]['Dist']) for i in range(len(targets_distractors))]
+    targets_distractors['q_dist1'] = [get_quad(targets_distractors.iloc[i]['Dist_NT1']) for i in range(len(targets_distractors))]
+    targets_distractors['q_dist2'] = [get_quad(targets_distractors.iloc[i]['Dist_NT2']) for i in range(len(targets_distractors))]
+    ##
+    targets_alone_quadrant=[]
+    distractor_alone_quadrant=[]
+    ###
+    for i in range(len(targets_distractors)):
+        targets_quadrants = [targets_distractors['q_t1'].iloc[i], targets_distractors['q_nt1'].iloc[i], targets_distractors['q_nt2'].iloc[i]]                                                                                 
+        distractors_quadrants = [targets_distractors['q_dist'].iloc[i], targets_distractors['q_dist1'].iloc[i], targets_distractors['q_dist2'].iloc[i]] 
+        ##################
+        ################## get target alone
+        ##################
+        if targets_quadrants[0] not in distractors_quadrants:
+            targets_alone_quadrant.append(targets_distractors.iloc[i]['T'])
+        elif targets_quadrants[1] not in distractors_quadrants:
+            targets_alone_quadrant.append(targets_distractors.iloc[i]['NT1'])
+        elif targets_quadrants[2] not in distractors_quadrants:
+            targets_alone_quadrant.append(targets_distractors.iloc[i]['NT2'])
+        else:
+            print('Error distribution stimuli')
+        ##################
+        ################## get distractor alone
+        ##################
+        if distractors_quadrants[0] not in targets_quadrants:
+            distractor_alone_quadrant.append(targets_distractors.iloc[i]['Dist'])
+        elif distractors_quadrants[1] not in targets_quadrants:
+            distractor_alone_quadrant.append(targets_distractors.iloc[i]['Dist_NT1'])
+        elif distractors_quadrants[2] not in targets_quadrants:
+            distractor_alone_quadrant.append(targets_distractors.iloc[i]['Dist_NT2'])
+        else:
+            print('Error distribution stimuli')
+    ###
+    ###
+    behaviour['T_alone'] = targets_alone_quadrant
+    behaviour['dist_alone'] = distractor_alone_quadrant
+    #
+    return behaviour
+
 
 
 
