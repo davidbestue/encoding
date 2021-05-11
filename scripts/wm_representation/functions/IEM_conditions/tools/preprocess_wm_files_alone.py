c2b3ea7d2b658075cb88a9b1f5d652cbba553ba7
David Bestue <davidsanchezbestue@hotmail.com>
2021-05-11 12:43:08 +0200

    update

--- scripts/wm_representation/functions/IEM_conditions/tools/preprocess_wm_files_alone.py
+++ scripts/wm_representation/functions/IEM_conditions/tools/preprocess_wm_files_alone.py
@@ -8,7 +8,7 @@ Created on Mon Apr 29 13:05:33 2019
 #import time
 
 def preprocess_wm_files_alone(wm_fmri_paths, masks, wm_beh_paths, condition, distance='mix', sys_use='unix', nscans_wm=16, TR=2.335,
-    time=time, multiprocessing=multiprocessing):
+    time, multiprocessing):
     ### Mask and process the fmri data
     start_process_wm = time.time()
     numcores = multiprocessing.cpu_count()
