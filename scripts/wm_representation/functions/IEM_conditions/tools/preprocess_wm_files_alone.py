10e24e3e47cc54ca1188480bf3c7fdec8a1644e9
David Bestue <davidsanchezbestue@hotmail.com>
2021-05-11 15:30:49 +0200

    update

--- scripts/wm_representation/functions/IEM_conditions/tools/preprocess_wm_files_alone.py
+++ scripts/wm_representation/functions/IEM_conditions/tools/preprocess_wm_files_alone.py
@@ -4,7 +4,7 @@ Created on Mon Apr 29 13:05:33 2019
 
 @author: David
 """
-
+import time
 
 def preprocess_wm_files_alone(wm_fmri_paths, masks, wm_beh_paths, condition, 
     distance='mix', sys_use='unix', nscans_wm=16, TR=2.335):
