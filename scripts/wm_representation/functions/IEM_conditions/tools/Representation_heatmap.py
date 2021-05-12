4afc85e449eecbf3a236e23e12d1853d9ca69e6a
David Bestue <davidsanchezbestue@hotmail.com>
2021-05-12 15:53:09 +0200

    update

--- scripts/wm_representation/functions/IEM_conditions/tools/Representation_heatmap.py
+++ scripts/wm_representation/functions/IEM_conditions/tools/Representation_heatmap.py
@@ -4,15 +4,87 @@ sys.path.insert(1, path_tools)
 from tools import *
 
 
-def Representation_heatmap(df, ref_angle=180, condition='1_7'):
-    plt.figure()
-    plt.title('Heatmap decoding')
-    ######midpoint = df.values.mean() # (df.values.max() - df.values.min()) / 2
-    ax = sns.heatmap(df, yticklabels=list(df.index), cmap="coolwarm") # cmap= viridis "jet",  "coolwarm" RdBu_r, gnuplot, YlOrRd, CMRmap  , center = midpoint
-    ax.plot([0.25, np.shape(df)[1]-0.25], [posch1_to_posch2(18),posch1_to_posch2(18)], 'k--')
-    plt.yticks([posch1_to_posch2(4), posch1_to_posch2(13), posch1_to_posch2(22), posch1_to_posch2(31)] ,['45','135','225', '315'])
-    plt.ylabel('Angle')
-    plt.xlabel('time (s)')
-    plt.show(block=False)
+def Representation_heatmap(df, condition='1_0.2', ref_angle=180, TR=2.335, bin_=0.5, fs1=20, fs2=15):
+    ##
+    ##
+    ##### pal_cyan = sns.color_palette("RdBu_r", n_colors=200)[40:] #RdBu_r
+    pal_cyan = 'viridis'
+    #
+    plt.figure(figsize=(8,6))
+    ax = sns.heatmap(df, cmap=pal_cyan, vmin=-0.2, vmax=0.2,  cbar=True, 
+                cbar_kws={"shrink": .82, 'ticks' : [-0.2, -0.1, 0, 0.1, 0.2], 'label': 'reconstruction activation (a.u)'})
+    ax.figure.axes[-1].yaxis.label.set_size(fs1)
+    ax.figure.axes[-1].tick_params(labelsize=fs2)
     
+    plt.gca().set_ylabel('')
+    plt.yticks([posch1_to_posch2(4), posch1_to_posch2(13), posch1_to_posch2(22), posch1_to_posch2(31)],['45','135','225', '315'])
+
+    dims=np.shape(df)
+    dimN = dims[0]
+    plt.gca().set_ylim(dimN, -45)
+    
+    plt.plot([1,1], [posch1_to_posch2(18),posch1_to_posch2(18)], markersize=10, color='r', marker='>')
+    
+    ################################
+    presentation_period= 0.35 #stim presnetation time
+    presentation_period_cue=  0.50 #presentation of attentional cue time
+    pre_stim_period= 0.5 #time between cue and stim
+    resp_time = 4  #time the response is active
+    start_hrf = 4  #start of the Hemodynamic response (4seconds)
+    if condition == '1_0.2':
+        condition_title = 'o:1, d:0.2'
+        delay1 = 0.2
+        delay2 = 11.8
+        cue=0
+        t_p = cue + presentation_period_cue + pre_stim_period 
+        d_p = t_p + presentation_period +delay1 
+        r_t = d_p + presentation_period + delay2
+        xlim = [1/TR, 30/TR]
+    elif condition=='1_7':
+        condition_title = 'o:1, d:7'
+        delay1 = 7
+        delay2 = 5
+        cue=0
+        t_p = cue + presentation_period_cue + pre_stim_period 
+        d_p = t_p + presentation_period +delay1 
+        r_t = d_p + presentation_period + delay2
+        xlim = [1/TR, 30/TR]
+    elif condition=='2_0.2':
+        condition_title = 'o:2, d:0.2'
+        delay1 = 0.2
+        delay2 = 12
+        cue=0
+        d_p = cue + presentation_period_cue + pre_stim_period 
+        t_p = d_p + presentation_period +delay1 
+        r_t = t_p + presentation_period + delay2   
+        xlim = [1/TR, 30/TR]
+    elif condition=='2_7':
+        condition_title = 'o:2, d:7'
+        delay1 = 7
+        delay2 = 12
+        cue=0
+        d_p = cue + presentation_period_cue + pre_stim_period 
+        t_p = d_p + presentation_period +delay1 
+        r_t = t_p + presentation_period + delay2
+        xlim = [1/TR, 35/TR]
+    ##
+    ##
+    t_p1 = (start_hrf + t_p) ## strat of target (time)
+    d_p1 = (start_hrf + d_p) ##strat of didtractor (time)
+    r_p1=  (start_hrf + r_t) ## start of response (time)
+    ##
+    plt.plot([t_p1/TR +bin_,t_p1/TR+bin_], [-15,-15], markersize=10, color='b', marker='v')
+    plt.plot([d_p1/TR +bin_,d_p1/TR+bin_], [-15,-15], markersize=10, color='g', marker='v')
+    plt.plot([r_p1/TR +bin_,r_p1/TR+bin_], [-15,-15], markersize=10, color='y', marker='v')
+    plt.gca().set_xlim(xlim)
+    
+    plt.xticks(fontsize=fs2)
+    plt.yticks(fontsize=fs2)
+    plt.gca().set_ylabel('angle ($^\circ$)', fontsize=fs1)
+    plt.gca().set_xlabel('time (s)', fontsize=fs1)
+    #plt.title(condition_title)
+    
+    plt.show()
+
+
     
