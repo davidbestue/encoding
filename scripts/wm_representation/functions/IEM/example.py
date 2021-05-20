f75924a6fa6d8e860c09280c532c66c3db7bb479
David Bestue <davidsanchezbestue@hotmail.com>
2021-05-20 11:02:45 +0200

    update

---
+++ scripts/wm_representation/functions/IEM/example.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  1 18:24:32 2019
+
+@author: David Bestue
+"""
+
+# Add to sys path the path where the tools folder ir (in this case, I need to go one back)
+import sys, os
+path_tools = os.getcwd() #os.path.abspath(os.path.join(os.getcwd(), os.pardir)) 
+sys.path.insert(1, path_tools)
+from tools import *
+
+##paths to save the files 
+path_save_signal ='/home/david/Desktop/Reconstructions/IEM/IEM_trainT_testT.xlsx' 
+path_save_shuffle = '/home/david/Desktop/Reconstructions/IEM/shuff_IEM_IEM_trainT_testT.xlsx'
+
+
+## TESTING OPTIONS
+decoding_thing = 'T_alone' #'dist_alone'  'T_alone'  
+
+## TRAINING OPTIONS
+training_item = 'T_alone'  ## 'dist_alone'  'T_alone' 
+cond_t = '1_7' # '1_7'  '2_7'
+
+Distance_to_use = 'mix' #'close' 'far'
+training_time= 'delay' #'stim_p'  'delay' 'respo'
+# depending on the options, the TRs used for the training will be different
+if training_time=='delay':
+    tr_st=4
+    tr_end=6
+
+
+## dictionary and list to save the files
+Reconstructions={}
+Reconstructions_shuff=[]
+
+## elements for the loop
+Conditions=['1_0.2', '1_7', '2_0.2', '2_7'] #, '1_7']
+Subjects=['d001', 'n001', 'b001', 'r001', 's001', 'l001']
+brain_regions = ['visual', 'ips', 'pfc']
+ref_angle=180
+
+num_shuffles = 5 #100 #10
+
+for Subject in Subjects:
+    for Brain_region in brain_regions:
+        for idx_c, Condition in enumerate(Conditions):
+            print(Subject, Brain_region, Condition )
+            #                    
+            if Condition == cond_t:  ### Cross validate if training and testing in the same condition
+                #get the data
+                enc_fmri_paths, enc_beh_paths, wm_fmri_paths, wm_beh_paths, masks = data_to_use( Subject, 'together', Brain_region)
+                #############
+                ###### Process wm files (I call them activity and not training_ or testing_ as it is the same data
+                activity, behaviour = preprocess_wm_data(wm_fmri_paths, masks, wm_beh_paths, 
+                    condition=Condition, distance=Distance_to_use, sys_use='unix', nscans_wm=nscans_wm, TR=2.335)
+                #############
+                ####### IEM 
+                Reconstruction = IEM_cv_all(testing_activity=activity, testing_behaviour=behaviour,
+                 decode_item=decoding_thing, training_item=training_item, tr_st=tr_st, tr_end=tr_end, n_slpits=10)
+                #
+                Reconstructions[Subject + '_' + Brain_region + '_' + Condition]=Reconstruction
+                #
+                # IEM shuffle
+                shuff = IEM_cv_all_shuff(testing_activity=activity, testing_behaviour=behaviour, 
+                    decode_item=decoding_thing, training_item=training_item, tr_st=tr_st, tr_end=tr_end,
+                    condition=Condition, subject=Subject, region=Brain_region,
+                    iterations=num_shuffles, n_slpits=10)
+                Reconstructions_shuff.append(shuff)
+                
+            else:
+                #get the data
+                enc_fmri_paths, enc_beh_paths, wm_fmri_paths, wm_beh_paths, masks = data_to_use( Subject, 'together', Brain_region)
+                ##################
+                ###### Process training data
+                training_activity, training_behaviour = preprocess_wm_data(wm_fmri_paths, masks, wm_beh_paths, 
+                    condition=cond_t, distance=Distance_to_use, sys_use='unix', nscans_wm=nscans_wm, TR=2.335)
+                #
+                # Subset training activity (TRs of  the time and column of beh for the training)
+                delay_TR_cond, training_thing = subset_training(training_activity=training_activity, training_behaviour=training_behaviour, 
+                    training_item=training_item , training_time=training_time, tr_st=tr_st, tr_end=tr_end)
+                ##################
+                ##### Train your weigths
+                WM, Inter = Weights_matrix_LM( delay_TR_cond, training_thing )
+                WM_t = WM.transpose()
+                ##################
+                ###### Process testing data 
+                testing_activity, testing_behaviour = preprocess_wm_data(wm_fmri_paths, masks, wm_beh_paths, 
+                    condition=Condition, distance=Distance_to_use, sys_use='unix', nscans_wm=nscans_wm, TR=2.335)
+                ##################
+                ###### IEM 
+                Reconstruction = IEM(testing_activity=testing_activity, testing_behaviour=testing_behaviour, decode_item=decoding_thing, 
+                    WM=WM, WM_t=WM_t, Inter=Inter, tr_st=tr_st, tr_end=tr_end)
+                #
+                Reconstructions[Subject + '_' + Brain_region + '_' + Condition]=Reconstruction
+                ##################
+                ###### IEM shuffle
+                shuff = IEM_shuff(testing_activity=testing_activity,testing_behaviour=testing_behaviour, decode_item=decoding_thing, 
+                    WM=WM, WM_t=WM_t, Inter=Inter, tr_st=tr_st, tr_end=tr_end, 
+                    condition=Condition, subject=Subject, region=Brain_region,
+                    iterations=num_shuffles, ref_angle=180)
+                #
+                Reconstructions_shuff.append(shuff)
+                
+
+
+        
+### Save signal         
+### Get signal from the reconstructions (get the signal before; not done in the function in case you want to save the whole)
+### If you want to save the whole recosntruction, uncomment the following lines
+
+### Save Recosntructions
+# path_save_reconstructions = #
+# writer = pd.ExcelWriter(path_save_reconstructions)
+# for i in range(len(Reconstructions.keys())):
+#     Reconstructions[Reconstructions.keys()[i]].to_excel(writer, sheet_name=Reconstructions.keys()[i]) #each dataframe in a excel sheet
+
+# writer.save()   #save reconstructions (heatmaps)
+
+#Save just the signal (around the decoding thing)
+Decoding_df =[]
+
+for dataframes in Reconstructions.keys():
+    df = Reconstructions[dataframes]
+    a = pd.DataFrame(df.iloc[ref_angle*2,:]) ##*2 because there are 720
+    a = a.reset_index()
+    a.columns = ['times', 'decoding'] # column names
+    a['decoding'] = [sum(df.iloc[:,i] * f2(ref_angle)) for i in range(len(a))] #"population vector method" scalar product
+    a['times']=a['times'].astype(float)
+    a['region'] = dataframes.split('_')[1]
+    a['subject'] = dataframes.split('_')[0]
+    a['condition'] = dataframes.split('_')[-2] + '_' + dataframes.split('_')[-1] 
+    Decoding_df.append(a)
+
+
+Df = pd.concat(Decoding_df)
+Df['label'] = 'signal' #ad the label of signal (you will concatenate this df with the one of the shuffleing)
+Df.to_excel( path_save_signal ) #save signal
+
+### Save Shuffle (in shuffles you do not need to get the *2 thing becuase it is done inside the function)
+Df_shuffs = pd.concat(Reconstructions_shuff)
+Df_shuffs['label'] = 'shuffle' ## add the label of shuffle
+Df_shuffs.to_excel(path_save_shuffle)  #save shuffle
+
+
