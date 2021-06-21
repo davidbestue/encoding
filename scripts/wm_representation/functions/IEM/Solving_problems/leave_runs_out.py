fcd4f82d127818d4a1bb967e9314b9194941426d
David Bestue <davidsanchezbestue@hotmail.com>
2021-06-21 12:13:03 +0200

    update

--- scripts/wm_representation/functions/IEM/Solving_problems/leave_runs_out.py
+++ scripts/wm_representation/functions/IEM/Solving_problems/leave_runs_out.py
@@ -61,7 +61,7 @@ Reconstructions={}
 Reconstructions_shuff=[]
 
 ############# Elements for the loop
-Conditions=['1_0.2'] #, '1_7', '2_0.2', '2_7'] 
+Conditions=['1_7'] #, '1_7', '2_0.2', '2_7'] 
 Subjects=['d001'] #, 'n001', 'b001', 'r001', 's001', 'l001']
 brain_regions = ['visual'] #, 'ips', 'pfc']
 ref_angle=180
