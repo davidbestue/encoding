e2508be4f0519d0396d10228cf0a01397ba68955
David Bestue <davidsanchezbestue@hotmail.com>
2021-08-16 12:38:46 +0200

    update

--- scripts/wm_representation/functions/IEM/Decoding_distractor/trainD_testD_r.py
+++ scripts/wm_representation/functions/IEM/Decoding_distractor/trainD_testD_r.py
@@ -12,10 +12,10 @@ sys.path.insert(1, path_tools)
 from tools import *
 
 ############# Namefiles for the savings. 
-path_save_signal ='/home/david/Desktop/Reconstructions/IEM/IEM_trainD_testD_runs.xlsx' 
-path_save_reconstructions = '/home/david/Desktop/Reconstructions/IEM/IEM_heatmap_trainD_testD_runs.xlsx'
+path_save_signal ='/home/david/Desktop/Reconstructions/IEM/IEM_trainD_testD_runsout.xlsx' 
+path_save_reconstructions = '/home/david/Desktop/Reconstructions/IEM/IEM_heatmap_trainD_testD_runsout.xlsx'
 
-path_save_shuffle = '/home/david/Desktop/Reconstructions/IEM/shuff_IEM_trainD_testD_runs.xlsx'
+path_save_shuffle = '/home/david/Desktop/Reconstructions/IEM/shuff_IEM_trainD_testD_runsout.xlsx'
 
 
 ############# Testing options
