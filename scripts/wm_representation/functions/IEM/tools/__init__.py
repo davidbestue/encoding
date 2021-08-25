ce4d9a73e08407c5ccac508ae8cc536a9f4f738c
David Bestue <davidsanchezbestue@hotmail.com>
2021-08-25 14:03:25 +0200

    update

--- scripts/wm_representation/functions/IEM/tools/__init__.py
+++ scripts/wm_representation/functions/IEM/tools/__init__.py
@@ -105,8 +105,8 @@ from IEM_all_runsout import *
 from IEM_all_runsout_shuff import *
 from IEM_cv_all_runsout_performance import *
 from IEM_cv_all_runsout_performance_shuff import *
+from IEM_all_runsout_beh import *
 from IEM_all_runsout_beh_shuff import *
-from IEM_all_runsout_shuff import *
 from IEM_cv_all_runsout_performance_responded import *
 from IEM_cv_all_runsout_performance_responded_shuff import *
 
