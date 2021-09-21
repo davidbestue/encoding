2298d0e41f87a32185891accfad8b66dcb94e356
David Bestue <davidsanchezbestue@hotmail.com>
2021-09-21 12:13:16 +0200

    update

--- scripts/wm_representation/functions/IEM/tools/Representation_angle.py
+++ scripts/wm_representation/functions/IEM/tools/Representation_angle.py
@@ -186,13 +186,15 @@ def decoding_angles_pvector( testing_behaviour, testing_data, df_shuffle, specif
     #
     for idx_tar, Dec_item in enumerate(['T', 'NT1', 'NT2']):
         #
+        testing_angles = np.array(testing_behaviour[Dec_item])   
+        testing_distractors = np.array(testing_behaviour[distractor_labels[idx_tar]])
+        #
         corresp_isol = list(np.array(testing_behaviour[Dec_item] == testing_behaviour['T_alone']) )
         corresp_isol_dist = list(np.array(testing_behaviour[distractor_labels[idx_tar]] == testing_behaviour['dist_alone']) )
         list_label_target = [Dec_item for i in range(len(testing_behaviour))]
         list_label_distr = [distractor_labels[idx_tar] for i in range(len(testing_behaviour))]
         new_indexes = list(testing_behaviour['new_index'].values)  
-        #
-        testing_angles = np.array(testing_behaviour[Dec_item]) 
+        #        
         Channel_all_trials_rolled = Parallel(n_jobs = numcores)(delayed(trial_rep)(Signal, angle_trial, Weights, Weights_t, ref=ref_angle, intercept_ = intercept)  for Signal, angle_trial in zip( data_prall, testing_angles))    ####
         Channel_all_trials_rolled = np.array(Channel_all_trials_rolled)
         n_trials_ = len(Channel_all_trials_rolled)
@@ -338,7 +340,7 @@ def Representation_angle_runsout(training_activity, training_behaviour, testing_
         for Idx in reconstrction_.new_index.unique():
             for Dec_item in ['T', 'NT1', 'NT2']:
                 df_x = reconstrction_.loc[(reconstrction_['new_index']==Idx) &  (reconstrction_['label_target']==Idx)]
-                decoded_angle = df_x.decoded_angle.nanmean()
+                decoded_angle = df_x.decoded_angle.mean() ###this ignores the Nans. It is the same as np.nanmean(df_x.decoded_angle.values) 
                 target_centered = df_x.targ_180.iloc[0]
                 label_target = df_x.label_target.iloc[0]
                 corresp_isolated = df_x.corresp_isolated.iloc[0]
@@ -391,141 +393,6 @@ df_shuffle=shuff
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-n_trials_test = len(testing_data) #number trials
-data_prall = []
-for i in range(n_trials_test):
-    data_prall.append(testing_data[i, :])
-
-
-###
-###
-numcores = multiprocessing.cpu_count()
-###
-###
-distractor_labels = ['Dist',  'Dist_NT1',  'Dist_NT2']
-###
-###
-frames=[]
-    #
-for idx_tar, Dec_item in enumerate(['T', 'NT1', 'NT2']):
-    #
-    corresp_isol = list(np.array(testing_behaviour[Dec_item] == testing_behaviour['T_alone']) )
-    corresp_isol_dist = list(np.array(testing_behaviour[distractor_labels[idx_tar]] == testing_behaviour['dist_alone']) )
-    list_label_target = [Dec_item for i in range(len(testing_behaviour))]
-    list_label_distr = [distractor_labels[idx_tar] for i in range(len(testing_behaviour))]
-    new_indexes = list(testing_behaviour['new_index'].values)  
-    testing_angles = np.array(testing_behaviour[Dec_item]) 
-    testing_angles
-        #
-
-
-
-
-testing_angles = np.array(testing_behaviour[Dec_item]) 
-Channel_all_trials_rolled = Parallel(n_jobs = numcores)(delayed(trial_rep)(Signal, angle_trial, Weights, Weights_t, ref=ref_angle, intercept_ = intercept)  for Signal, angle_trial in zip( data_prall, testing_angles))    ####
-Channel_all_trials_rolled = np.array(Channel_all_trials_rolled)
-n_trials_ = len(Channel_all_trials_rolled)
-        ### 
-### substract the shuffle and just get positive data (rest to 0) ##chequear que esto funcione...
-Channel_all_trials_substracted = Channel_all_trials_rolled - df_shuffle[str(specific_tr_shuffle*TR) ].values ##chequear que esto funcione...
-Channel_all_trials_substracted[Channel_all_trials_substracted<0]=0
-###
-###
-distance_to_ref =  testing_angles - ref_angle
-targ_180 = testing_angles - distance_to_ref
-        ##
-        ##
-decoded_angle=[]
-for i in range(n_trials_):
-    Trial_reconstruction = Channel_all_trials_substracted[i,:]
-
-
-Trial_reconstruction = Channel_all_trials_substracted[-1,:]
-_135_ = ref_angle*2 - 45*2 
-_225_ = ref_angle*2 + 45*2 
-Trial_135_225 = Trial_reconstruction[_135_:_225_]
-N=len(Trial_135_225)
-R = []
-angles = np.radians(np.linspace(135,224,180) ) 
-R=np.dot(Trial_135_225,np.exp(1j*angles)) / N
-angle = np.angle(R)
-if angle < 0:
-    angle +=2*np.pi 
-
-#
-#
-
-angle_degrees = np.degrees(angle)
-
-
-if np.all(Trial_135_225==0)==True: ### if all the values are negative here, the decoded is np.nan (will not count as 0)
-    angle_degrees=np.nan
-
-
-angle_degrees 
-
-
-
-    decoded_angle.append(angle_degrees)
-        ###
-        ###
-        decoded_angle=np.array(decoded_angle)
-        #### Rotation of distractor associated
-        distractors_angles = np.array(testing_behaviour[distractor_labels[idx_tar]]) 
-        #
-        dist_180_ = testing_distractors - distance_to_ref
-        dist_180 = []
-        for n_dist in dist_180_:
-            if n_dist<0:
-                n_ = n_dist+360
-                dist_180.append(n_)
-            elif n_dist>360:
-                n_ = n_dist-360
-                dist_180.append(n_)
-            else:
-                dist_180.append(n_dist)
-        ##
-        dist_180 = np.array(dist_180)
-        #
-        df_dec = pd.DataFrame({'decoded_angle':decoded_angle, 'target_centered':targ_180, 
-                                'label_target':list_label_target, 'corresp_isolated':corresp_isol,
-                                'distractor_centered':dist_180, 'corresp_isolated_distractor':corresp_isol_dist,
-                                'label_distractor':list_label_distr, 'new_index':new_indexes })
-        #
-        frames.append(df_dec)
-    #
-    df_dec = pd.concat(frames)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 list_wm_scans= range(nscans_wm)  
 list_wm_scans2 = list_wm_scans
 ####
@@ -545,43 +412,24 @@ for not_shared in list_wm_scans2:
     ###########################################################################
     ########################################################################### Get the mutliple indexes to split in train and test
     ###########################################################################
-    training_indexes = []
-    testing_indexes =  []
-    for sess_run in testing_behaviour.session_run.unique():
-        wanted = testing_behaviour.loc[testing_behaviour['session_run']==sess_run].index.values 
-        testing_indexes.append( wanted )
-        #
-        ## I do not trust the del  lines of other files, maybe this del inside a function in paralel is not removing the indexes, also you avoid going to lists to comeback
-        all_indexes = testing_behaviour.index.values
-        other_indexes = all_indexes[~np.array([all_indexes[i] in wanted for i in range(len(all_indexes))])]  #take the ones that are not in wanted
-        training_indexes.append( other_indexes ) 
-    ###
-    ### apply them to train and test
-    ###
+
+
+training_indexes = []
+testing_indexes =  []
+for sess_run in testing_behaviour.session_run.unique():
+    wanted = testing_behaviour.loc[testing_behaviour['session_run']==sess_run].index.values 
+    testing_indexes.append( wanted )
+    #
+    ## I do not trust the del  lines of other files, maybe this del inside a function in paralel is not removing the indexes, also you avoid going to lists to comeback
+    all_indexes = testing_behaviour.index.values
+    other_indexes = all_indexes[~np.array([all_indexes[i] in wanted for i in range(len(all_indexes))])]  #take the ones that are not in wanted
+    training_indexes.append( other_indexes ) 
+        ###
+        ### apply them to train and test
+        ###
 for train_index, test_index in zip(training_indexes, testing_indexes):
     X_train, X_test = training_data[train_index], testing_data[test_index]
     y_train, y_test = training_angles[train_index], testing_angles[test_index]
-        ## train
-
-
-
-WM2, Inter2 = Weights_matrix_LM(X_train, y_train)
-WM_t2 = WM2.transpose()
-## test
-trials_test = testing_behaviour.iloc[test_index, :]
-
-
-
-rep_x = decoding_angles_pvector( testing_behaviour=trials_test, testing_data=X_test, df_shuffle=df_shuffle, 
-                specific_tr_shuffle=not_shared, Weights=WM2, Weights_t=WM_t2, ref_angle=180, intercept=Inter2)
-
-
-
-testing_behaviour=trials_test
-testing_data=X_test
-df_shuffle=df_shuffle
-specific_tr_shuffle=not_shared
-Weights=WM2
-Weights_t=WM_t2
-ref_angle=180
-intercept=Inter2
+    ## train
+    WM2, Inter2 = Weights_matrix_LM(X_train, y_train)
+    WM_t2 = WM2.transpose()
