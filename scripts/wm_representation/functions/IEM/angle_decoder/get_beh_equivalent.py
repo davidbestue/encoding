2357f43e1e9e51bd102396f1568961a4076f1385
David Bestue <davidsanchezbestue@hotmail.com>
2021-10-08 13:01:42 +0200

    update

--- scripts/wm_representation/functions/IEM/angle_decoder/get_beh_equivalent.py
+++ scripts/wm_representation/functions/IEM/angle_decoder/get_beh_equivalent.py
@@ -52,7 +52,7 @@ tr_end=6
 Reconstruction_angles=[]
 
 ############# Elements for the loop
-Conditions=['1_0.2'] #, '1_7', '2_0.2', '2_7'] 
+Conditions=['1_0.2', '1_7'] #, '2_0.2', '2_7'] 
 Subjects=['d001'] #, 'n001', 'b001', 'r001', 's001', 'l001']
 brain_regions = ['visual'] #, 'ips', 'pfc']
 ref_angle=180
